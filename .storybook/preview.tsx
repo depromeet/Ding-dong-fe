e4d535b51b3f95f04879a3fdfd1d9fc3b5881c23
yunho <kimyouknow@naver.com>
2023-05-24 14:18:35 +0900

    Feat: react-query 설치
    - storybook decorators 설정

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -3,6 +3,10 @@ const path = require('path');
 import React from 'react';
 import { initialize, mswDecorator } from 'msw-storybook-addon';
 import type { Preview } from '@storybook/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+// TODO: Provider폴더 구조 정해지면 수정해야합니다!
+const queryClient = new QueryClient();
 
 import '../src/app/globals.css';
 
@@ -10,7 +14,14 @@ import '../src/app/globals.css';
 initialize();
 
 const preview: Preview = {
-  decorators: [mswDecorator, Story => <Story />],
+  decorators: [
+    mswDecorator,
+    Story => (
+      <QueryClientProvider client={queryClient}>
+        <Story />
+      </QueryClientProvider>
+    ),
+  ],
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
