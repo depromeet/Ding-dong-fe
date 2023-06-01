87990ed8b5cf2be9eb226e9924763d1cdc2cd8d4
yunho <kimyouknow@naver.com>
2023-06-01 16:59:56 +0900

    Feat: tailwind config에 content 폴더 수정
    pages제거
    modules추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,9 +18,9 @@ const declareSpace = () => {
 
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/modules/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
