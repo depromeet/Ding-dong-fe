7f38046cf3c392bcdfd71e0ade0d9b6f8f5dc019
yunho <kimyouknow@naver.com>
2023-06-01 16:59:16 +0900

    Feat: tailwind config 유틸함수 추가
    pxToRem: 픽셀을 rem으로 변환하는 함수
    declareSpace: 픽셀단위의 space를 rem으로 변환

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+
+const pxToRem = (px, base = 16) => `${px / base}rem`;
+
+/**
+ * 픽셀 단위의 space(margin, padding) 값을 선언하고, 해당 값들의 rem 단위 변환 값을 반환합니다.
+ * ex) key: 2px -> value: "0.125rem"
+ * @returns {Object} 픽셀 값들을 키로 가지고 해당 값들의 rem 단위 변환 값을 값으로 가지는 객체입니다.
+ */
+const declareSpace = () => {
+  const numbers = [2, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52];
+  const spacing = numbers.reduce((acc, px) => {
+    acc[`${px}px`] = pxToRem(px);
+    return acc;
+  }, {});
+  return spacing;
+};
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
