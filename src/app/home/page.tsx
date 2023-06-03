6b57ece983b217dd4b99b154372b9031965d90ca
darae07 <leedr0730@gmail.com>
2023-06-03 22:46:03 +0900

    feat: add session redirect

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,18 +1,22 @@
 import SignIn from '@/app/api/auth/signin/page';
+import SessionRedirect from '@/app/api/auth/signin/sessionRedirect';
 
 const Home = () => {
   return (
-    <div className="flex h-screen flex-col justify-between px-6 pb-20 pt-28">
-      <div>
-        <div className="text-4xl">LOGO</div>
-        <div className="mt-8 flex h-72 w-full items-center justify-center rounded-full bg-gray-100 text-center">
-          graphic
+    <>
+      <SessionRedirect />
+      <div className="flex h-screen flex-col justify-between px-6 pb-20 pt-28">
+        <div>
+          <div className="text-4xl">LOGO</div>
+          <div className="mt-8 flex h-72 w-full items-center justify-center rounded-full bg-gray-100 text-center">
+            graphic
+          </div>
         </div>
-      </div>
 
-      {/* @ts-expect-error Server Component */}
-      <SignIn />
-    </div>
+        {/* @ts-expect-error Server Component */}
+        <SignIn />
+      </div>
+    </>
   );
 };
 
