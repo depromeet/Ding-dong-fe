8266624e879b5ab45e9f0030324dbfb7c2fa3fe8
darae07 <leedr0730@gmail.com>
2023-06-03 22:33:52 +0900

    refactor: fix lint error

--- src/app/Provider.tsx
+++ src/app/Provider.tsx
@@ -3,7 +3,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { SessionProvider } from 'next-auth/react';
-import { SessionProvider } from 'next-auth/react';
 import { useState } from 'react';
 
 import initMocks from '@/mocks';
