513922c3dfb3a1c5dcea5d3f00c1bc1ad3dede4d
99-zziy <wldud8229@gmail.com>
2023-06-05 00:28:16 +0900

    Fix: portal 새로고침시 안보이는 에러 수정

--- src/components/Portal/Portal.tsx
+++ src/components/Portal/Portal.tsx
@@ -1,23 +1,28 @@
 'use client';
 
 import { AnimatePresence, motion } from 'framer-motion';
-import { PropsWithChildren } from 'react';
-import ReactDOM from 'react-dom';
+import { PropsWithChildren, useEffect, useRef } from 'react';
+import { createPortal } from 'react-dom';
 
 import useIsMounted from '@/hooks/useIsMounted';
 
 const Portal = ({ children }: PropsWithChildren) => {
+  const ref = useRef<Element | null>(null);
   const isMounted = useIsMounted();
-  const element = document.getElementById('portal');
 
-  if (!element || !isMounted) return null;
-  return ReactDOM.createPortal(
+  useEffect(() => {
+    ref.current = document.getElementById('portal');
+  }, [isMounted]);
+
+  if (!(isMounted && ref.current)) return null;
+
+  return createPortal(
     <AnimatePresence>
       <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
         {children}
       </motion.div>
     </AnimatePresence>,
-    element,
+    ref.current,
   );
 };
 
