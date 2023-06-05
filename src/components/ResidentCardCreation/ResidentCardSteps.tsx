0b8929fb02881aedce6645809fc39a931155b77a
hyehyeon-moon <hyehyeon.moon@linecorp.com>
2023-06-05 11:16:31 +0900

    Feat: Add ResidentCardSteps component

---
+++ src/components/ResidentCardCreation/ResidentCardSteps.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { useCallback, useState } from 'react';
+import { FormProvider, useForm } from 'react-hook-form';
+
+import { Steps } from '@/components/ResidentCardCreation/Form';
+
+const steps: Steps[] = ['BOARDING', 'PROFILE', 'KEYWORD', 'KEYWORD_CONTENT', 'COMPLETE'];
+
+export const ResidentCardSteps = () => {
+  const methods = useForm();
+  const [stepOrder, setStepOrder] = useState<number>(0);
+  const onNext = useCallback(() => {
+    setStepOrder(stepOrder + 1);
+  }, [stepOrder]);
+  const onPrev = useCallback(() => {
+    setStepOrder(stepOrder - 1);
+  }, [stepOrder]);
+
+  return (
+    <FormProvider {...methods}>
+      {steps[stepOrder] === 'BOARDING' && (
+        <div>
+          <div>borading</div>
+          <button onClick={onNext}>행성 만들기</button>
+        </div>
+      )}
+      {['PROFILE', 'KEYWORD', 'KEYWORD_CONTENT'].includes(steps[stepOrder]) && <div>form</div>}
+      {steps[stepOrder] === 'COMPLETE' && <div>complete</div>}
+    </FormProvider>
+  );
+};
