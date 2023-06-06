ab145e04b4aff9ae2d7bf201a0c8ac0aad0ca673
hyehyeon-moon <hyehyeon.moon@linecorp.com>
2023-06-06 23:52:05 +0900

    Feat: Fix the number of characters that go beyond the limit when it is Korean

--- src/components/ResidentCardCreation/Step/ProfileStep.client.tsx
+++ src/components/ResidentCardCreation/Step/ProfileStep.client.tsx
@@ -6,11 +6,13 @@ const fieldTitleStyle = 'text-b2  text-grey-500';
 const fieldStyle =
   'border-[0.5px] border-solid mt-2px rounded-[6px] border-grey-100 bg-grey-50 w-full text-b1 focus:outline-none focus:border-primary-500';
 const title = '이웃 주민에게\n 자신을 소개해주세요!';
+const TEXT_MAX_LENGTH = 50;
 
 export const ProfileStep = () => {
   const { register } = useFormContext();
   const [textCount, setTextCount] = useState(0);
   const onTextareaHandler = useCallback(e => {
+    e.target.value = e.target.value.slice(0, TEXT_MAX_LENGTH);
     setTextCount(e.target.value.length);
   }, []);
   return (
@@ -24,7 +26,7 @@ export const ProfileStep = () => {
       />
       <div className={`${fieldTitleStyle} mt-16px`}>소개</div>
       <textarea
-        {...register('aboutMe', { maxLength: 50 })}
+        {...register('aboutMe', { maxLength: TEXT_MAX_LENGTH })}
         onChange={onTextareaHandler}
         className={`${fieldStyle} p-14px`}
       />
