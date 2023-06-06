f9aa10fc171d4494b011cefd9f773e304413f3e2
darae07 <leedr0730@gmail.com>
2023-06-06 15:13:48 +0900

    style: apply animation

--- src/components/BottomSheet/BottomSheet.tsx
+++ src/components/BottomSheet/BottomSheet.tsx
@@ -10,36 +10,37 @@ import { BottomSheetWrapper } from '@/components/BottomSheet/BottomSheetWrapper'
 import { UseBottomSheetReturn } from '@/components/BottomSheet/useBottomSheet';
 import Portal from '@/components/Portal';
 
+const WrapperVariants = {
+  initial: { opacity: 0 },
+  animate: { opacity: 1 },
+  exit: { opacity: 0, transition: { duration: 0.7 } },
+};
+
+const SheetVariants = {
+  initial: { y: 300, opacity: 0 },
+  animate: { y: 0, opacity: 1 },
+  transition: { duration: 0.5, delay: 0.1, ease: [0, 0.71, 0.2, 1.01] },
+  exit: { y: 300, opacity: 0, transition: { duration: 0.5 } },
+};
+
 export type BottomSheetProps = PropsWithChildren & UseBottomSheetReturn;
 const BottomSheet = ({ children, isOpen, onClose }: BottomSheetProps) => {
   return (
     <Portal>
-      {isOpen && (
-        <AnimatePresence>
-          <motion.div
-            key="wrapper"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-          >
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div key="wrapper" {...WrapperVariants}>
             <BottomSheetWrapper onClose={onClose}></BottomSheetWrapper>
+            <motion.div
+              key="sheet"
+              {...SheetVariants}
+              className="fixed bottom-0 left-0 w-full rounded-t-[20px] bg-white px-5 py-6"
+            >
+              {children}
+            </motion.div>
           </motion.div>
-          <motion.div
-            key="sheet"
-            initial={{ y: 300, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            transition={{
-              duration: 0.5,
-              delay: 0.5,
-              ease: [0, 0.71, 0.2, 1.01],
-            }}
-            exit={{ y: 300, opacity: 0 }}
-            className="fixed bottom-0 left-0 w-full rounded-t-[20px] bg-white px-5 py-6"
-          >
-            {children}
-          </motion.div>
-        </AnimatePresence>
-      )}
+        )}
+      </AnimatePresence>
     </Portal>
   );
 };
