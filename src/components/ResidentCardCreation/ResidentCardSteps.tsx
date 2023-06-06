7ac49cf18fb1fb9be238f245f39248efa857632e
문혜현 <55529617+hyehyeonmoon@users.noreply.github.com>
2023-06-07 01:08:55 +0900

    Add ResidentCardSteps and ResidentCardForm (#16)
    * Feat: Add ResidentCardSteps component
    
    * Feat: Add ResidentCardForm component and story
    
    * Feat: Remove unused dark mode
    
    * Feat: Move common type from ResidentCardForm to ResidentCardCreation.type.ts
    
    * Feat: Remove any type and make build success
    
    * Bug: Remove unnecessary prettier option
    
    ---------
    
    Co-authored-by: hyehyeon-moon <hyehyeon.moon@linecorp.com>

---
+++ src/components/ResidentCardCreation/ResidentCardSteps.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { useCallback, useState } from 'react';
+import { FormProvider, useForm } from 'react-hook-form';
+
+import { ResidentCardForm } from '@/components/ResidentCardCreation/Form';
+import { Steps } from '@/components/ResidentCardCreation/ResidentCardCreation.type';
+
+const steps: Steps[] = ['BOARDING', 'PROFILE', 'KEYWORD', 'KEYWORD_CONTENT', 'COMPLETE'];
+
+export const ResidentCardSteps = () => {
+  const methods = useForm();
+  const [stepOrder, setStepOrder] = useState<number>(0);
+  const onNext = useCallback(() => {
+    setStepOrder(stepOrder + 1);
+  }, [stepOrder]);
+  const onPrev = useCallback(() => {
+    setStepOrder(stepOrder - 1);
+  }, [stepOrder]);
+
+  return (
+    <FormProvider {...methods}>
+      {steps[stepOrder] === 'BOARDING' && (
+        <div>
+          <div>borading</div>
+          <button onClick={onNext}>행성 만들기</button>
+        </div>
+      )}
+      {['PROFILE', 'KEYWORD', 'KEYWORD_CONTENT'].includes(steps[stepOrder]) && (
+        <div>
+          <ResidentCardForm steps={steps} stepOrder={stepOrder} onNext={onNext} onPrev={onPrev} />
+        </div>
+      )}
+      {steps[stepOrder] === 'COMPLETE' && <div>complete</div>}
+    </FormProvider>
+  );
+};
