d22a22faf722f6a9b99331fbb2aa88a836106d84
YUNHO <kimyouknow@naver.com>
2023-06-06 22:49:46 +0900

    Dep 56 Top Navigation (#17)
    * Feat: ArrowLeft Icon
    
    * Feat: Cancel Icon
    
    * Feat: ChevronLeft Icon
    
    * Feat: Icon.index.ts
    
    * Feat: icon strories
    
    * Feat: TopNavigation
    
    * Feat: BackButton 종류 추가
    
    chevron
    cancel
    arrow
    
    * Refactor: title -> titleElement
    
    * Feat: custom back link
    
    * Feat: bottom border Color
    
    * Feat: tailwind config z-index 추가
    
    * Feat: TopNavigation z-index top1 적용
    
    * Chore: pnpm-lock
    
    * Feat: padding추가
    
    * Refactor  story 네이밍 수정

---
+++ src/components/TopNavigation/TopNavigation.stories.tsx
@@ -0,0 +1,61 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import TopNavigation from './index';
+
+const meta: Meta<typeof TopNavigation> = {
+  title: 'TopNavigation',
+  component: TopNavigation,
+  args: {},
+  parameters: {
+    // storybook에서 useRouter를 쓰기 위한 설정
+    nextjs: {
+      appDirectory: true,
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof TopNavigation>;
+
+export const Primary: Story = {
+  render: () => <TopNavigation />,
+};
+
+export const CancelBackButton: Story = {
+  render: () => <TopNavigation backButtonType="cancel" />,
+};
+
+export const BackButtonWithCustomLink: Story = {
+  render: () => <TopNavigation backLink="/custom-link" />,
+};
+
+export const TitleWithDBackButton: Story = {
+  render: () => <TopNavigation titleElement="Title" />,
+};
+
+export const TitleWithTwoLine: Story = {
+  render: () => (
+    <TopNavigation
+      titleElement={
+        <div className="flex flex-col items-center justify-center">
+          <span className="text-b3 text-grey-500">김피피</span>
+          <span className="text-grey-900">주민정보</span>
+        </div>
+      }
+    />
+  ),
+};
+
+export const BottomBorder: Story = {
+  render: () => <TopNavigation titleElement="Title" bottomBorderColor="primary-500" />,
+};
+
+export const TitleWithRightButton: Story = {
+  render: () => (
+    <TopNavigation
+      titleElement="Title"
+      rightButtonElement={<button className="text-h5 text-primary-500">다음</button>}
+    />
+  ),
+};
