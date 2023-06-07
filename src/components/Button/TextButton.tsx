c5d082745e863cc5b6beb327c4a5c367bfb30c77
99-zziy <wldud8229@gmail.com>
2023-06-07 17:01:20 +0900

    Feat: TextButton 수정

--- src/components/Button/TextButton.tsx
+++ src/components/Button/TextButton.tsx
@@ -2,10 +2,7 @@ import { ButtonHTMLAttributes, PropsWithChildren } from 'react';
 
 export type TextButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & PropsWithChildren;
 
-const TextButton = ({
-  children,
-  ...props
-}: ButtonHTMLAttributes<HTMLButtonElement> & PropsWithChildren) => {
+const TextButton = ({ children, ...props }: TextButtonProps) => {
   return <button {...props}>{children}</button>;
 };
 
