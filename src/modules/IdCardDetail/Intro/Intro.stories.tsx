f3f3ac1e98985f248f6f847abbf8b06866599e83
99-zziy <wldud8229@gmail.com>
2023-06-08 22:44:22 +0900

    Feat: Intro 스토리 작성

---
+++ src/modules/IdCardDetail/Intro/Intro.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Intro } from './Intro.server';
+
+const meta: Meta<typeof Intro> = {
+  title: 'Intro',
+  component: Intro,
+};
+
+type Story = StoryObj<typeof Intro>;
+
+export const Default: Story = {
+  render: () => (
+    <Intro
+      idCardId={'1'}
+      aboutMe="안녕하세요 저는 N년차 개발자구요 :) 너무 보고싶었어요 다들 잘부탁드립니다!"
+      nickname="이트루"
+      characterType="TRUE"
+      profileImageUrl=""
+      keywords={[
+        { keywordId: 1, title: '엽기떡기', imageUrl: '', content: '' },
+        { keywordId: 1, title: 'FE 신입', imageUrl: '', content: '' },
+        { keywordId: 1, title: '르세라핌 최고', imageUrl: '', content: '' },
+      ]}
+    />
+  ),
+};
+
+export default meta;
