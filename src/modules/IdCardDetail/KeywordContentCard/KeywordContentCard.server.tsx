47f67be02b54bd8aea81209c8d6680a7a0dbcf01
99-zziy <wldud8229@gmail.com>
2023-06-09 01:06:19 +0900

    Refactor: TODO 주석추가

--- src/modules/IdCardDetail/KeywordContentCard/KeywordContentCard.server.tsx
+++ src/modules/IdCardDetail/KeywordContentCard/KeywordContentCard.server.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import { KeywordType } from '@/types/id-cards.type';
 
 export const KeywordContentCard = ({ keywordId, title, imageUrl, content }: KeywordType) => {
+  // TODO: 이미지 사진 사이즈 조절
   return (
     <div className="flex w-full flex-col gap-2.5 rounded-xl bg-grey-100 px-3.5 py-2.5">
       <p className="text-base font-semibold text-grey-900">{title}</p>
@@ -11,8 +12,8 @@ export const KeywordContentCard = ({ keywordId, title, imageUrl, content }: Keyw
           <Image
             src={imageUrl}
             alt={title}
-            width={300}
-            height={300}
+            width={308}
+            height={192}
             className="rounded-xl object-cover"
           />
         )}
