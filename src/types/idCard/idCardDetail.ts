fb99d9e40855513580e29a0449fd7dea2706bd60
darae07 <leedr0730@gmail.com>
2023-06-08 23:40:51 +0900

    feat: add CharacterType & add cSpell

--- src/types/idCard/idCardDetail.ts
+++ src/types/idCard/idCardDetail.ts
@@ -3,6 +3,8 @@ import { Keyword } from './keyword';
 export type CharacterType = {
   PIPI: { group: 'FIRST'; korean: '피피'; english: 'Pipi' };
   LUNA: { group: 'FIRST'; korean: '루나'; english: 'Luna' };
+  TOBBY: { group: 'FIRST'; korean: '토비'; english: 'Tobby' };
+  BUDDY: { group: 'FIRST'; korean: '버디'; english: 'Buddy' };
 };
 
 export type IdCardDetail = {
