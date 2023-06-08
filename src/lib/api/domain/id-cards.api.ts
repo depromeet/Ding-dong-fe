0367d1a97b2c6aa197a4382ab822fcd556806577
99-zziy <wldud8229@gmail.com>
2023-06-08 21:34:07 +0900

    Feat: 주민증 세부내용 api 추가

---
+++ src/lib/api/domain/id-cards.api.ts
@@ -0,0 +1,12 @@
+import { BaseResponseType, GetIdCardDetailType } from '@/lib/api/config/api.types';
+import publicApi from '@/lib/api/config/publicApi';
+import { REQUEST_API } from '@/lib/api/config/requestUrl';
+
+const getIdCardDetail = (id: number) =>
+  publicApi.get<BaseResponseType<GetIdCardDetailType>>(`${REQUEST_API.IDCARDS.DETAIL}/${id}`);
+
+const idCardApi = {
+  getIdCardDetail,
+};
+
+export default idCardApi;
