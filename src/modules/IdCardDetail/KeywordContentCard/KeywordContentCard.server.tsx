9a362ce193c677b84125230ca8d228eff814fe56
hyehyeon-moon <hyehyeon.moon@linecorp.com>
2023-06-10 02:30:43 +0900

    Feat: Add ReactNode prop to KeywordContentCard

--- src/modules/IdCardDetail/KeywordContentCard/KeywordContentCard.server.tsx
+++ src/modules/IdCardDetail/KeywordContentCard/KeywordContentCard.server.tsx
@@ -1,22 +1,18 @@
-import Image from 'next/image';
+import { ReactNode } from 'react';
 
-import { KeywordType } from '@/types/id-cards.type';
+type KeywordContentCardProps = {
+  title: ReactNode | string;
+  image: ReactNode | null;
+  content: ReactNode | string;
+};
 
-export const KeywordContentCard = ({ keywordId, title, imageUrl, content }: KeywordType) => {
+export const KeywordContentCard = ({ title, image, content }: KeywordContentCardProps) => {
   // TODO: 이미지 사진 사이즈 조절
   return (
     <div className="flex w-full flex-col gap-2.5 rounded-xl bg-grey-100 px-3.5 py-2.5">
       <p className="text-base font-semibold text-grey-900">{title}</p>
       <div className="flex flex-col gap-2">
-        {imageUrl && (
-          <Image
-            src={imageUrl}
-            alt={title}
-            width={308}
-            height={192}
-            className="rounded-xl object-cover"
-          />
-        )}
+        {image}
         <p className="text-[15px] font-normal text-grey-800">{content}</p>
       </div>
     </div>
