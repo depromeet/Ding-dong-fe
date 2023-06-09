88c4a3cd0ed10de515855925198f815cec0110d3
darae07 <leedr0730@gmail.com>
2023-06-10 01:11:00 +0900

    feat: add server url

--- src/lib/api/config/requestUrl.ts
+++ src/lib/api/config/requestUrl.ts
@@ -1,7 +1,7 @@
 import { isProd } from '@/utils/util.common';
 
 // TODO: API 배포 후 설정 필요
-const DEVELOPMENT_API_URL = '/api';
+const DEVELOPMENT_API_URL = 'https://ding-dong-planet.com/api';
 const PRODUCTION_API_URL = '/api';
 
 export const ROOT_API_URL = isProd(process.env.NODE_ENV) ? PRODUCTION_API_URL : DEVELOPMENT_API_URL;
