a23829017fc4bf3b191bd94abe07b551fda2eb57
hyehyeon-moon <hyehyeon.moon@linecorp.com>
2023-06-10 00:44:39 +0900

    Feat: Add label for image input

--- src/modules/IdCardCreation/Step/KeywordContentStep.client.tsx
+++ src/modules/IdCardCreation/Step/KeywordContentStep.client.tsx
@@ -1,9 +1,7 @@
 'use client';
 import { useFieldArray, useFormContext } from 'react-hook-form';
 
-import { Data } from '@/modules/IdCardCreation/IdCardSteps';
-
-const title = '이웃 주민에게 자신을 소개할\n 키워드를 적어주세요!';
+const title = '나를 소개하는 키워드의\n 설명을 적어주세요!';
 
 export const KeywordContentStep = () => {
   const { control, register } = useFormContext<Data>();
@@ -12,15 +10,29 @@ export const KeywordContentStep = () => {
   return (
     <div>
       <h1 className="text-h1">{title}</h1>
-      {fields.map((field, index) => {
-        return (
-          <div key={field.id}>
-            <div className="bg-red-100">{field.title}</div>
-            <textarea {...register(`keywords.${index}.content`)} />
-            <input {...register(`keywords.${index}.imageUrl`)} type="file" accept="image/*" />
-          </div>
-        );
-      })}
+      <div>
+        {fields.map((field, index) => {
+          return (
+            <div key={field.id}>
+              <div>{field.title}</div>
+              <textarea {...register(`keywords.${index}.content`)} />
+              <label
+                htmlFor={`keywords.${index}.imageUrl`}
+                className="text-primary font rounded-[12px] border-[0.5px] border-solid border-grey-100 bg-grey-50 px-[6px] pb-[6px] pt-[6px] text-detail text-primary-500"
+              >
+                이미지 변경
+              </label>
+              <input
+                id={`keywords.${index}.imageUrl`}
+                {...register(`keywords.${index}.imageUrl`)}
+                type="file"
+                accept="image/*"
+                className="hidden"
+              />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
