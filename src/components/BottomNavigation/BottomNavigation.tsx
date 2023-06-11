14e8146ffad33993af663ad9604d2401f7e2aa39
99-zziy <wldud8229@gmail.com>
2023-06-11 11:47:28 +0900

    Feat: Divider 추가

--- src/components/BottomNavigation/BottomNavigation.tsx
+++ src/components/BottomNavigation/BottomNavigation.tsx
@@ -2,6 +2,7 @@
 
 import { usePathname, useRouter } from 'next/navigation';
 
+import { Divider } from '@/components/Divider';
 import { BellIcon, HomeIcon, PersonIcon } from '@/components/Icon';
 
 type BottomNavigationPath = '/communities' | '/notification' | '/my-page';
@@ -23,15 +24,18 @@ export const BottomNavigation = () => {
   };
 
   return (
-    <nav className="flex h-[52px] items-center justify-evenly">
-      <div onClick={() => handleNavigation('/communities')}>
-        <HomeIcon className={getSvgcolor('/communities')} />
-      </div>
-      <div onClick={() => handleNavigation('/notification')}>
-        <BellIcon height={26} className={getSvgcolor('/notification')} />
-      </div>
-      <div onClick={() => handleNavigation('/my-page')}>
-        <PersonIcon className={getSvgcolor('/my-page')} />
+    <nav>
+      <Divider />
+      <div className="flex h-[52px] items-center justify-evenly">
+        <div onClick={() => handleNavigation('/communities')}>
+          <HomeIcon className={getSvgcolor('/communities')} />
+        </div>
+        <div onClick={() => handleNavigation('/notification')}>
+          <BellIcon height={26} className={getSvgcolor('/notification')} />
+        </div>
+        <div onClick={() => handleNavigation('/my-page')}>
+          <PersonIcon className={getSvgcolor('/my-page')} />
+        </div>
       </div>
     </nav>
   );
