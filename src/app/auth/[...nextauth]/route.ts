410b404d61da80e5bbd340d7399bb406f6f03475
darae07 <leedr0730@gmail.com>
2023-06-11 15:45:26 +0900

    fix: fix build error  "authOptions" is not a valid Route export field.

--- src/app/auth/[...nextauth]/route.ts
+++ src/app/auth/[...nextauth]/route.ts
@@ -11,33 +11,31 @@ const cookies: Partial<CookiesOptions> = {
   },
 };
 
-export const authOptions: AuthOptions = {
-  providers: [
-    KakaoProvider({
-      clientId: process.env.KAKAO_CLIENT_ID ?? '',
-      clientSecret: process.env.KAKAO_CLIENT_SECRET ?? '',
-      authorization: 'https://kauth.kakao.com/oauth/authorize',
-    }),
-  ],
-  cookies: cookies,
-  callbacks: {
-    async jwt({ token, account }) {
-      if (account?.accessToken) {
-        token.accessToken = account.accessToken;
-      }
-      return token;
-    },
-    async session({ session }) {
-      return session;
-    },
-  },
-  pages: {
-    signIn: '/auth/signin',
-  },
-};
-
 const auth = async (req: NextRequest, res: NextApiResponse) => {
-  return NextAuth(req as unknown as NextApiRequest, res, authOptions);
+  return NextAuth(req as unknown as NextApiRequest, res, {
+    providers: [
+      KakaoProvider({
+        clientId: process.env.KAKAO_CLIENT_ID ?? '',
+        clientSecret: process.env.KAKAO_CLIENT_SECRET ?? '',
+        authorization: 'https://kauth.kakao.com/oauth/authorize',
+      }),
+    ],
+    cookies: cookies,
+    callbacks: {
+      async jwt({ token, account }) {
+        if (account?.accessToken) {
+          token.accessToken = account.accessToken;
+        }
+        return token;
+      },
+      async session({ session }) {
+        return session;
+      },
+    },
+    pages: {
+      signIn: '/auth/signin',
+    },
+  });
 };
 
 export { auth as GET, auth as POST };
