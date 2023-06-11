71708af0b989c73a1d8998a4e5d6b246b996617b
yunho <kimyouknow@naver.com>
2023-06-11 12:35:35 +0900

    Feat: TextAreaImage

---
+++ src/components/TextArea/TextAreaImage.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+
+import { tw } from '@/utils/tailwind.util';
+
+type SafeNumber = number | `${number}`;
+
+type TextAreaImageProps = { src: string; alt: string; height?: SafeNumber };
+
+export const TextAreaImage = ({ src, alt, height }: TextAreaImageProps) => {
+  return (
+    <div className={tw(`py-8pxr`)}>
+      <Image
+        src={src}
+        alt={alt}
+        width={0}
+        height={0}
+        style={{ width: '100%', height: height ?? 'auto' }}
+      />
+    </div>
+  );
+};
