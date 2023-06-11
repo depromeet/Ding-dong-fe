71453992153caa87113c3e1aac6a82cd0a14853c
darae07 <leedr0730@gmail.com>
2023-06-11 14:26:57 +0900

    refactor: remove unused variable

--- src/app/accounts/page.tsx
+++ src/app/accounts/page.tsx
@@ -1,14 +1,10 @@
 import 'server-only';
 
-import { cookies } from 'next/headers';
-
 import { ROOT_API_URL } from '@/lib/api/config/requestUrl';
 
 import CallApiTest from './callApiTest';
 
 const AccountHome = async () => {
-  const cookieStore = cookies();
-  const userId = cookieStore.get('userId');
   const communities = await fetch(`${ROOT_API_URL}/communities/1/idCards`);
 
   console.log('response is ok?', communities.ok);
