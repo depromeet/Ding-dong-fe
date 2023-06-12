51278dbdb8ca9ef5b1a999f44c4e20589dc97c70
99-zziy <wldud8229@gmail.com>
2023-06-12 21:17:27 +0900

    Feat: layout에 bottom nav 적용

--- src/app/communities/[id]/layout.tsx
+++ src/app/communities/[id]/layout.tsx
@@ -1,7 +1,14 @@
 import { PropsWithChildren } from 'react';
 
+import { BottomNavigation } from '@/components/BottomNavigation';
+
 const Layout = ({ children }: PropsWithChildren) => {
-  return <div className="p-[27px]">{children}</div>;
+  return (
+    <div className="p-[27px]">
+      {children}
+      <BottomNavigation />
+    </div>
+  );
 };
 
 export default Layout;
--- src/app/communities/[id]/page.tsx
+++ src/app/communities/[id]/page.tsx
@@ -5,6 +5,7 @@ import { dehydrate, Hydrate } from '@tanstack/react-query';
 import { community } from '@/hooks/api/queryKey.type';
 import communityApi from '@/lib/api/domain/community.api';
 import getQueryClient from '@/lib/tanstackQuery/getQueryClient';
+import { CommunityDetail } from '@/modules/CommunityDetail';
 import { CommunityIdCards } from '@/modules/CommunityIdCards';
 
 const page = async () => {
@@ -22,6 +23,7 @@ const page = async () => {
   return (
     <Hydrate state={dehydratedState}>
       <div>
+        <CommunityDetail />
         <h3 className="mb-16pxr text-h3 text-grey-800">{'우리 행성 주민을 소개할게요!'}</h3>
         <CommunityIdCards />
       </div>
