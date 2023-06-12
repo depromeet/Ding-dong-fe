5b762bc2b9365efa257109009560deed49e40fa8
yunho <kimyouknow@naver.com>
2023-06-13 00:29:43 +0900

    Refactor: TextAreaContent에서 memo제거

--- src/components/TextArea/TextAreaContent.tsx
+++ src/components/TextArea/TextAreaContent.tsx
@@ -1,29 +1,27 @@
-/* eslint-disable react/prop-types */
 'use client';
-import { forwardRef, memo, TextareaHTMLAttributes } from 'react';
+import { forwardRef, TextareaHTMLAttributes } from 'react';
 
 import { tw } from '@/utils/tailwind.util';
 
 type TextAreaContentProps = TextareaHTMLAttributes<HTMLTextAreaElement> & object;
 
-export const TextAreaContent = memo(
-  forwardRef<HTMLTextAreaElement, TextAreaContentProps>(
-    ({ name, placeholder, value, onChange, onBlur, disabled, ...rest }, ref) => {
-      const disabledCss = disabled && 'cursor-not-allowed';
-      return (
-        <textarea
-          id={`text-area-${name}`}
-          ref={ref}
-          className={tw('mt-8pxr w-full bg-inherit', disabledCss)}
-          placeholder={placeholder}
-          value={value}
-          onChange={onChange}
-          onBlur={onBlur}
-          disabled={disabled}
-          autoComplete="off"
-          {...rest}
-        />
-      );
-    },
-  ),
+// eslint-disable-next-line react/display-name
+export const TextAreaContent = forwardRef<HTMLTextAreaElement, TextAreaContentProps>(
+  ({ name, placeholder, value, onChange, onBlur, disabled, ...rest }, ref) => {
+    const disabledCss = disabled && 'cursor-not-allowed';
+    return (
+      <textarea
+        id={`text-area-${name}`}
+        ref={ref}
+        className={tw('mt-8pxr w-full bg-inherit', disabledCss)}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+        disabled={disabled}
+        autoComplete="off"
+        {...rest}
+      />
+    );
+  },
 );
