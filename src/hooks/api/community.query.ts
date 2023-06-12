01393e51f1ee7766af53d17e60772c27b1f220da
99-zziy <wldud8229@gmail.com>
2023-06-12 20:01:10 +0900

    Feat: enabled:false로 한 이유 주석 추가

--- src/hooks/api/community.query.ts
+++ src/hooks/api/community.query.ts
@@ -12,6 +12,7 @@ export const useGetCommunityIdCards = ({ id, pageParam }: ComunityIdCardsRequest
       getNextPageParam: data =>
         !data.communityIdCardsDtos.hasNext ? data.communityIdCardsDtos.page + 1 : undefined,
       refetchOnWindowFocus: false,
+      //NOTE: 서버컴포넌트에서 이미 1페이지를 데이터 fetch 했기 때문에 2페이지 부터 fetch 하기 위함입니다.
       enabled: false,
     },
   );
