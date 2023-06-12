ea4b1b1d8537e905024b8441af3bb118d872b48c
yunho <kimyouknow@naver.com>
2023-06-12 21:54:10 +0900

    Refactor: 스토리 합성컴포넌트 반영

--- src/components/TopNavigation/TopNavigation.stories.tsx
+++ src/components/TopNavigation/TopNavigation.stories.tsx
@@ -1,17 +1,12 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import TopNavigation from './index';
+import { TopNavigation } from './index';
 
 const meta: Meta<typeof TopNavigation> = {
   title: 'TopNavigation',
   component: TopNavigation,
   args: {},
-  parameters: {
-    // storybook에서 useRouter를 쓰기 위한 설정
-    nextjs: {
-      appDirectory: true,
-    },
-  },
+  parameters: {},
 };
 
 export default meta;
@@ -19,43 +14,77 @@ export default meta;
 type Story = StoryObj<typeof TopNavigation>;
 
 export const Primary: Story = {
-  render: () => <TopNavigation />,
+  render: () => <TopNavigation></TopNavigation>,
 };
 
 export const CancelBackButton: Story = {
-  render: () => <TopNavigation backButtonType="cancel" />,
+  render: () => (
+    <TopNavigation>
+      <TopNavigation.Left>
+        <TopNavigation.BackButton backButtonType="cancel" />
+      </TopNavigation.Left>
+    </TopNavigation>
+  ),
 };
 
 export const BackButtonWithCustomLink: Story = {
-  render: () => <TopNavigation backLink="/custom-link" />,
+  render: () => (
+    <TopNavigation>
+      <TopNavigation.Left>
+        <TopNavigation.BackButton backLink="/custom-link" />
+      </TopNavigation.Left>
+    </TopNavigation>
+  ),
 };
 
 export const TitleWithDBackButton: Story = {
-  render: () => <TopNavigation titleElement="Title" />,
+  render: () => (
+    <TopNavigation>
+      <TopNavigation.Left>
+        <TopNavigation.BackButton backLink="/custom-link" />
+      </TopNavigation.Left>
+      <TopNavigation.Title>Title</TopNavigation.Title>
+    </TopNavigation>
+  ),
 };
 
 export const TitleWithTwoLine: Story = {
   render: () => (
-    <TopNavigation
-      titleElement={
+    <TopNavigation>
+      <TopNavigation.Left>
+        <TopNavigation.BackButton backLink="/custom-link" />
+      </TopNavigation.Left>
+      <TopNavigation.Title>
         <div className="flex flex-col items-center justify-center">
           <span className="text-b3 text-grey-500">김피피</span>
           <span className="text-grey-900">주민정보</span>
         </div>
-      }
-    />
+      </TopNavigation.Title>
+    </TopNavigation>
   ),
 };
 
 export const BottomBorder: Story = {
-  render: () => <TopNavigation titleElement="Title" bottomBorderColor="primary-500" />,
+  render: () => (
+    <TopNavigation bottomBorderColor="primary-500">
+      <TopNavigation.Left>
+        <TopNavigation.BackButton backLink="/custom-link" />
+      </TopNavigation.Left>
+      <TopNavigation.Title>Title</TopNavigation.Title>
+    </TopNavigation>
+  ),
 };
 
 export const TitleWithRightButton: Story = {
   render: () => (
-    <TopNavigation
-      titleElement="Title"
-      rightButtonElement={<button className="text-h5 text-primary-500">다음</button>}
-    />
+    <TopNavigation bottomBorderColor="primary-500">
+      <TopNavigation.Left>
+        <TopNavigation.BackButton />
+      </TopNavigation.Left>
+      <TopNavigation.Title>Title</TopNavigation.Title>
+      <TopNavigation.Right>
+        <button className="text-h5 text-primary-500">다음</button>
+      </TopNavigation.Right>
+    </TopNavigation>
   ),
 };
