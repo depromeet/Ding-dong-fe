cc4f4f3aeb529d20f39c7d03e8d8a3b36e8ce176
99-zziy <wldud8229@gmail.com>
2023-06-13 16:34:28 +0900

    Refactor: test 파일 삭제

--- src/api/domain/tester.api.ts
+++
@@ -1,8 +0,0 @@
-import publicApi from '~/api/config/publicApi';
-import { REQUEST_API } from '~/api/config/requestUrl';
-
-const testerApi = {
-  getTester: () => publicApi.get<{ tester: { id: string; nickname: string } }>(REQUEST_API.TESTER),
-};
-
-export default testerApi;
