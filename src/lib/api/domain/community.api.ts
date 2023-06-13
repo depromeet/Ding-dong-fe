3fc98f049cee31d5ad6bf373bccc6c269ee612d8
yunho <kimyouknow@naver.com>
2023-06-13 10:10:06 +0900

    Refactor: getCommunityIdCards에서 수정된 onResponse적용

--- src/lib/api/domain/community.api.ts
+++ src/lib/api/domain/community.api.ts
@@ -1,12 +1,8 @@
 import publicApi from '~/lib/api/config/publicApi';
 import { CommunityIdCardsResponse } from '~/types/community';
 
-const getCommunityIdCards = async (id: string, pageParam: number) => {
-  const res = await publicApi
-    .get<CommunityIdCardsResponse>(`/communities/${id}/idCards?page=${pageParam}&size=10`)
-    .catch(err => err.response.data);
-  return res;
-};
+const getCommunityIdCards = (id: string, pageParam: number) =>
+  publicApi.get<CommunityIdCardsResponse>(`/communities/${id}/idCards?page=${pageParam}&size=10`);
 
 const communityApi = {
   getCommunityIdCards,
