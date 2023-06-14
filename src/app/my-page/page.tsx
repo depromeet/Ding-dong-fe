4d81da8ab09d00e5505e937827d3f20b6e027a09
yunho <kimyouknow@naver.com>
2023-06-14 15:13:29 +0900

    Feat: my-page에서 설정 페이지로 넘어가기

--- src/app/my-page/page.tsx
+++ src/app/my-page/page.tsx
@@ -1,19 +1,28 @@
+'use client';
+import { useRouter } from 'next/navigation';
+
 import { GearFillIcon } from '~/components/Icon';
 import { TopNavigation } from '~/components/TopNavigation';
 import { createIdCard } from '~/mocks/idCard/idCard.mock';
 import { IdCard } from '~/modules/IdCard';
 
 const MyPage = () => {
+  const router = useRouter();
   // TODO: 해당 행성에서 내 주민증 정보 요청 api 추가 예정
   const { idCardId, nickname, aboutMe, characterType, keywords } = createIdCard();
   const keywordTitles = keywords.map(keyword => keyword.title);
+
+  const onClickGearFill = () => {
+    router.push('/my-page/config');
+  };
+
   return (
     <main>
-      <TopNavigation>
+      <TopNavigation bottomBorderColor="bg-grey-100">
         <TopNavigation.Left>커뮤니티 이동 컴포넌트</TopNavigation.Left>
         <TopNavigation.Title></TopNavigation.Title>
         <TopNavigation.Right>
-          <GearFillIcon />
+          <GearFillIcon onClick={onClickGearFill} />
         </TopNavigation.Right>
       </TopNavigation>
       <div className="mt-[86px]">
