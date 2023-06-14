9292f7be48a4f9c6576693b99b13fca4ffd64a6c
99-zziy <wldud8229@gmail.com>
2023-06-15 01:33:01 +0900

    Refactor: id -> idcard id로 수정

--- src/api/domain/idCard.api.ts
+++ src/api/domain/idCard.api.ts
@@ -1,5 +1,5 @@
 import privateApi from '~/api/config/privateApi';
 import { IdCardDetailResponse } from '~/types/idCard';
 
-export const getIdCardDetail = (id: string) =>
-  privateApi.get<IdCardDetailResponse>(`/id-cards/${id}`);
+export const getIdCardDetail = (idCardId: string) =>
+  privateApi.get<IdCardDetailResponse>(`/id-cards/${idCardId}`);
