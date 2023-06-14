22bc00a69fd9fb71fcdeb717dd995539ee1814e2
yunho <kimyouknow@naver.com>
2023-06-14 21:42:28 +0900

    Feat: EditProfileInfoStep form 로직

--- src/modules/IdCardEditor/Step/EditProfileInfoStep.client.tsx
+++ src/modules/IdCardEditor/Step/EditProfileInfoStep.client.tsx
@@ -2,13 +2,15 @@ import { useFormContext } from 'react-hook-form';
 
 import { TextArea, useTextArea } from '~/components/TextArea';
 import { TextInput, useTextInput } from '~/components/TextInput';
+import { IdCardEditorFormModel } from '~/types/idCard';
 
 const MAX_NICKNAME_LENGTH = 16;
 const MAX_ABOUT_ME_LENGTH = 50;
 
 export const EditProfileInfoStep = () => {
-  const { register, watch } = useFormContext();
-  const { nickname, aboutMe } = watch();
+  const { register, getValues } = useFormContext<IdCardEditorFormModel>();
+  const { nickname, aboutMe } = getValues();
+
   // TODO: TextInput, TextArea 안쪽으로 리팩토링해야 할듯
   const { textCount: nicknameCount, onChangeHandler: onChangeNickName } = useTextInput({
     initCount: nickname.length,
@@ -28,7 +30,10 @@ export const EditProfileInfoStep = () => {
           라벨
         </TextInput.Label>
         <TextInput.Border textCount={nicknameCount} maxLength={MAX_NICKNAME_LENGTH}>
-          <TextInput.Content {...register('nickname')} onChange={onChangeNickName} />
+          <TextInput.Content
+            {...register('nickname', { required: true })}
+            onChange={onChangeNickName}
+          />
         </TextInput.Border>
       </TextInput>
       <TextArea>
@@ -36,7 +41,10 @@ export const EditProfileInfoStep = () => {
           라벨
         </TextArea.Label>
         <TextArea.Border textCount={aboutMeCount} maxLength={MAX_ABOUT_ME_LENGTH}>
-          <TextArea.Content {...register('aboutMe')} onChange={onChangeAboutMe} />
+          <TextArea.Content
+            {...register('aboutMe', { required: true })}
+            onChange={onChangeAboutMe}
+          />
         </TextArea.Border>
       </TextArea>
     </div>
