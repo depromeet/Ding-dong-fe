f3ae28ce556ef179b3cc05d2bc15bbbd92c4931c
yunho <kimyouknow@naver.com>
2023-06-16 01:33:45 +0900

    Feat: editIdCardDetail api with mock handler

--- src/api/domain/idCard.api.ts
+++ src/api/domain/idCard.api.ts
@@ -1,5 +1,15 @@
 import privateApi from '~/api/config/privateApi';
-import { IdCardDetailResponse } from '~/types/idCard';
+import { EditIdCardRequest, IdCardDetailResponse } from '~/types/idCard';
 
 export const getIdCardDetail = (idCardId: string) =>
   privateApi.get<IdCardDetailResponse>(`/id-cards/${idCardId}`);
+
+export const editIdCardDetail = (idCardInfo: EditIdCardRequest) => {
+  const { idCardId, profileImageUrl, nickname, aboutMe, keywords } = idCardInfo;
+  return privateApi.put(`/id-cards/${idCardId}`, {
+    profileImageUrl,
+    nickname,
+    aboutMe,
+    keywords,
+  });
+};
--- src/mocks/idCard/idCard.mockHandler.ts
+++ src/mocks/idCard/idCard.mockHandler.ts
@@ -7,6 +7,9 @@ const idCardMockHandler = [
   rest.get(`${ROOT_API_URL}/id-cards/:idCardId`, (req, res, ctx) => {
     return res(ctx.status(200), ctx.json({ idCardDetailsDto: createIdCard() }));
   }),
+  rest.put(`${ROOT_API_URL}/id-cards/:idCardId`, (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({ id: createIdCard().idCardId }));
+  }),
 ];
 
 export default idCardMockHandler;
