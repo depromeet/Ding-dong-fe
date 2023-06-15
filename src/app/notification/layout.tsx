9539b277a28bb02138959fca667be33e3b9709dc
99-zziy <wldud8229@gmail.com>
2023-06-16 00:22:59 +0900

    Feat: top nav 행성 이름 보여주기

--- src/app/notification/layout.tsx
+++ src/app/notification/layout.tsx
@@ -1,10 +1,12 @@
 import { PropsWithChildren } from 'react';
 
 import { BottomNavigation } from '~/components/BottomNavigation';
+import { PlanetNameTopNavigation } from '~/modules/PlanetNameTopNavigation';
 
 const Layout = ({ children }: PropsWithChildren) => {
   return (
-    <div>
+    <div className="pt-t-nav">
+      <PlanetNameTopNavigation />
       {children}
       <BottomNavigation />
     </div>
--- src/app/planet/[id]/layout.tsx
+++ src/app/planet/[id]/layout.tsx
@@ -1,12 +1,12 @@
 import { PropsWithChildren } from 'react';
 
 import { BottomNavigation } from '~/components/BottomNavigation';
-import { TopNavigation } from '~/components/TopNavigation';
+import { PlanetNameTopNavigation } from '~/modules/PlanetNameTopNavigation';
 
 const Layout = ({ children }: PropsWithChildren) => {
   return (
-    <div className="pt-[44px]">
-      <TopNavigation />
+    <div className="pt-t-nav">
+      <PlanetNameTopNavigation />
       {children}
       <BottomNavigation />
     </div>
