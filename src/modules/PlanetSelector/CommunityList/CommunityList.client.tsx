b01867cfa56324efe7827534d90c5d4f2bc58c36
99-zziy <wldud8229@gmail.com>
2023-06-16 15:06:04 +0900

    Refactor: 커뮤니티 리스트 컴포넌트 분리

---
+++ src/modules/PlanetSelector/CommunityList/CommunityList.client.tsx
@@ -0,0 +1,44 @@
+'use client';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+
+import { useGetCommunityList } from '~/api/domain/community.api';
+import { Divider } from '~/components/Divider';
+import { CommunityListModel } from '~/types/community';
+import { tw } from '~/utils/tailwind.util';
+
+export const CommunityList = () => {
+  // TODO: userId 수정 필요
+  const userId = '1';
+  const { data: communityList } = useGetCommunityList(userId);
+  const router = useRouter();
+
+  return (
+    <ul className="rounded-xl border border-grey-200 bg-grey-50">
+      {communityList &&
+        communityList.communityListDtos.map((community: CommunityListModel) => (
+          <li key={community.communityId} onClick={() => router.push(`planet/${community.title}`)}>
+            <div className="flex items-center gap-20pxr p-20pxr">
+              <Image
+                width={36}
+                height={36}
+                src={community.logoImageUrl}
+                alt={`${community.title} logo image`}
+                style={{
+                  borderRadius: '50%',
+                  width: '36px',
+                  height: '36px',
+                  objectFit: 'cover',
+                }}
+              />
+              <div className="flex flex-col gap-4pxr">
+                <p className={`${tw('text-b1 text-[#282828]', 'font-bold')}`}>{community.title}</p>
+                <p className="text-b2 text-[#848484]">{community.idCardCount} 주민</p>
+              </div>
+            </div>
+            <Divider />
+          </li>
+        ))}
+    </ul>
+  );
+};
--- src/modules/PlanetSelector/PlanetSelector.client.tsx
+++ src/modules/PlanetSelector/PlanetSelector.client.tsx
@@ -1,20 +1,14 @@
 'use client';
-import Image from 'next/image';
 
-import { useGetCommunityList } from '~/api/domain/community.api';
 import { useBottomSheet } from '~/components/BottomSheet';
 import BottomSheet from '~/components/BottomSheet/BottomSheet';
-import { Divider } from '~/components/Divider';
 import { ArrowIcon, PlusIcon } from '~/components/Icon';
 import { TopNavigation } from '~/components/TopNavigation';
-import { CommunityListModel } from '~/types/community';
+import { CommunityList } from '~/modules/PlanetSelector/CommunityList';
 import { tw } from '~/utils/tailwind.util';
 
 export const PlanetSelector = () => {
-  // TODO: userId 수정 필요
-  const userId = '1';
   const bottomSheetHandlers = useBottomSheet();
-  const { data: communityList } = useGetCommunityList(userId);
 
   return (
     <div>
@@ -32,41 +26,14 @@ export const PlanetSelector = () => {
         <BottomSheet.Header>내 행성</BottomSheet.Header>
         <BottomSheet.Content>
           <div className="flex flex-col gap-20pxr">
-            <ul className="rounded-xl border border-grey-200 bg-grey-50">
-              {communityList &&
-                communityList.communityListDtos.map((community: CommunityListModel) => (
-                  <li key={community.communityId}>
-                    <div className="flex items-center gap-20pxr p-20pxr">
-                      <Image
-                        width={36}
-                        height={36}
-                        src={community.logoImageUrl}
-                        alt={`${community.title} logo image`}
-                        style={{
-                          borderRadius: '50%',
-                          width: '36px',
-                          height: '36px',
-                          objectFit: 'cover',
-                        }}
-                      />
-                      <div className="flex flex-col gap-4pxr">
-                        <p className={`${tw('text-b1 text-[#282828]', 'font-bold')}`}>
-                          {community.title}
-                        </p>
-                        <p className="text-b2 text-[#848484]">{community.idCardCount} 주민</p>
-                      </div>
-                    </div>
-                    <Divider />
-                  </li>
-                ))}
-            </ul>
+            <CommunityList />
             <div className="rounded-xl border border-grey-200 bg-grey-50">
-              <div className="flex items-center gap-20pxr px-20pxr py-16pxr">
+              <button className="flex items-center gap-20pxr px-20pxr py-16pxr">
                 <div className="flex h-[36px] w-[36px] items-center justify-center rounded-3xl bg-grey-100">
                   <PlusIcon />
                 </div>
                 <p className={`${tw('text-b1 text-[#282828]', 'font-bold')}`}>행성 만들기</p>
-              </div>
+              </button>
             </div>
           </div>
         </BottomSheet.Content>
