ee53042d0caadec0ad4764c15f45a91eb0b50889
hyehyeon-moon <hyehyeon.moon@linecorp.com>
2023-06-17 23:44:15 +0900

    Fix: Fix default export for keeping convention

--- src/mocks/idCard/idCard.mockHandler.ts
+++ src/mocks/idCard/idCard.mockHandler.ts
@@ -1,12 +1,9 @@
 import { rest } from 'msw';
 
 import { ROOT_API_URL } from '~/api/config/requestUrl';
-import { createIdCard } from '~/mocks/idCard/idCard.mock';
 
-const idCardMockHandler = [
+export const idCardMockHandler = [
   rest.get(`${ROOT_API_URL}/id-cards/:idCardId`, (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json({ idCardDetailsDto: createIdCard() }));
+    return res(ctx.status(200), ctx.json({ idCardDetailsDto: idCardDetailMock() }));
   }),
 ];
-
-export default idCardMockHandler;
