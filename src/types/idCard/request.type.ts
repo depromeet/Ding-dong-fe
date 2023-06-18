fa90a95f25bcdb66f9e17c959f3a4f56ea32cefe
hyehyeon-moon <hyehyeon.moon@linecorp.com>
2023-06-18 16:05:28 +0900

    Fix: Fix type error

--- src/types/idCard/request.type.ts
+++ src/types/idCard/request.type.ts
@@ -1,5 +1,6 @@
 import { IdCardCreationFormModel, IdCardEditorFormModel } from '~/types/idCard';
 
 export type CreateIdCardRequest = IdCardCreationFormModel;
+export type IdCardCreateRequest = IdCardCreationFormModel;
 
 export type EditIdCardRequest = IdCardEditorFormModel;
