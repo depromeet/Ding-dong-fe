a255417e88f1b8d5f263194e4a83bd686d402d1f
99-zziy <wldud8229@gmail.com>
2023-06-19 01:07:27 +0900

    Feat: 행성 이미지 수정

--- src/app/planet/page.tsx
+++ src/app/planet/page.tsx
@@ -7,9 +7,9 @@ import { PlanetEnterButtons } from '~/modules/PlanetEnterButtons';
 const PlanetPage = () => {
   return (
     <div className="flex h-screen flex-col justify-center px-5">
-      <h1 className="mb-4 mt-1 text-h2 text-grey-900">아직 소속된 행성이 없네요!</h1>
+      <h1 className="mb-12pxr mt-4pxr text-h2 text-grey-900">아직 소속된 행성이 없네요!</h1>
       <p className="text-b2 text-grey-700">행성을 만들거나 초대된 행성으로 이동해보세요.</p>
-      <Image src="/assets/images/planet.png" alt="planet" width={400} height={400} />
+      <Image src="/assets/images/n-planet.png" alt="planet" width={400} height={400} />
       <PlanetEnterButtons />
     </div>
   );
