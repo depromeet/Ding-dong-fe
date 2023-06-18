2c7f9a83c9b9270d785981d5b7827119aa8fcd37
99-zziy <wldud8229@gmail.com>
2023-06-19 01:08:58 +0900

    Feat: 초대 코드 입력하기 버튼 삭제

--- src/modules/PlanetEnterButtons/PlanetEnterButtons.client.tsx
+++ src/modules/PlanetEnterButtons/PlanetEnterButtons.client.tsx
@@ -5,7 +5,7 @@ import TextButton from '~/components/Button/TextButton';
 
 export const PlanetEnterButtons = () => {
   return (
-    <div className="-mt-10">
+    <div className="mt-6pxr">
       <TextButton
         className="w-full text-center text-b1 text-primary-500"
         onClick={() => console.log('딩동행성 둘러보기')}
@@ -15,9 +15,6 @@ export const PlanetEnterButtons = () => {
       <div className="mt-[60px] flex flex-col gap-4">
         <Button color="primary" size="large" onClick={() => console.log('행성 만들기')}>
           행성 만들기
-        </Button>
-        <Button color="secondary" size="large" onClick={() => console.log('초대코드 입력하기')}>
-          초대코드 입력하기
         </Button>
       </div>
     </div>
