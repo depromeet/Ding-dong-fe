e6179f94314353e123c563a697297663c1a60e81
99-zziy <wldud8229@gmail.com>
2023-06-19 01:15:10 +0900

    Feat: 소속된 행성이 없는 경우 바텀바 말고 환경설정 버튼 보이도록하기

--- src/app/planet/layout.tsx
+++ src/app/planet/layout.tsx
@@ -1,12 +1,27 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
 import { PropsWithChildren } from 'react';
 
-import { BottomNavigation } from '~/components/BottomNavigation';
+import { GearFillIcon } from '~/components/Icon';
+import { TopNavigation } from '~/components/TopNavigation';
 
 const Layout = ({ children }: PropsWithChildren) => {
+  const router = useRouter();
+  const onClickGearFill = () => {
+    router.push('/my-page/config');
+  };
+
   return (
     <div>
+      <TopNavigation bottomBorderColor="bg-grey-100">
+        <TopNavigation.Left />
+        <TopNavigation.Title />
+        <TopNavigation.Right>
+          <GearFillIcon onClick={onClickGearFill} className="fill-gray-700" />
+        </TopNavigation.Right>
+      </TopNavigation>
       {children}
-      <BottomNavigation />
     </div>
   );
 };
