b25930852658e13c070d6ab317046d64c78aa547
hyehyeon-moon <hyehyeon.moon@linecorp.com>
2023-06-18 22:25:18 +0900

    Feat: Add storybook cookie addon

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -7,6 +7,7 @@ import type { Preview } from '@storybook/react';
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import handlers from '../src/mocks/handlers';
+import { AUTH_COOKIE_KEYS } from '../src/types/auth/response.type';
 
 // TODO: Provider폴더 구조 정해지면 수정해야합니다!
 const queryClient = new QueryClient();
@@ -50,6 +51,13 @@ const preview: Preview = {
       };
 
       return config;
+    },
+    cookie: {
+      //  TODO: 개발용 token으로 변경, env.dev 사용
+      [AUTH_COOKIE_KEYS.accessToken]: '8888',
+      [AUTH_COOKIE_KEYS.refreshToken]: '8888',
+      [AUTH_COOKIE_KEYS.userId]: '8888',
+      [AUTH_COOKIE_KEYS.accessTokenExpireDate]: '8888',
     },
   },
 };
