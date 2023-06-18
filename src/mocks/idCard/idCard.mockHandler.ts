7da2d0153e6fed9d1419a6c150a64ea3e82a69fa
hyehyeon-moon <hyehyeon.moon@linecorp.com>
2023-06-18 14:40:26 +0900

    Fix: Add mock in broswer msw

--- src/mocks/idCard/idCard.mockHandler.ts
+++ src/mocks/idCard/idCard.mockHandler.ts
@@ -3,12 +3,13 @@ import { rest } from 'msw';
 import { ROOT_API_URL } from '~/api/config/requestUrl';
 import { createIdCardMock, idCardDetailMock } from '~/mocks/idCard/idCard.mock';
 
+export const idCardCreateMockHandler = rest.post(`${ROOT_API_URL}/id-cards`, (req, res, ctx) =>
+  res(ctx.status(200), ctx.json(createIdCardMock)),
+);
+
 export const idCardMockHandler = [
   rest.get(`${ROOT_API_URL}/id-cards/:idCardId`, (req, res, ctx) => {
     return res(ctx.status(200), ctx.json({ idCardDetailsDto: idCardDetailMock() }));
   }),
+  idCardCreateMockHandler,
 ];
-
-export const idCardCreateMockHandler = rest.post(`${ROOT_API_URL}/id-cards`, (req, res, ctx) =>
-  res(ctx.status(200), ctx.json(createIdCardMock)),
-);
