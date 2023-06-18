3367f50b90da4242ec7c404604f4daa3cb277019
yunho <kimyouknow@naver.com>
2023-06-19 00:12:44 +0900

    Feat: getEntries util

--- src/utils/util.common.ts
+++ src/utils/util.common.ts
@@ -1,1 +1,7 @@
 export const isProd = (env: string): boolean => env === 'production';
+
+type Entries<T> = {
+  [K in keyof T]: [K, T[K]];
+}[keyof T][];
+
+export const getEntries = <T extends object>(obj: T) => Object.entries(obj) as Entries<T>;
