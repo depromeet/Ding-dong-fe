9a8f8564044c28ab2327d3a32090d03a0ade9685
hyehyeon-moon <hyehyeon.moon@linecorp.com>
2023-06-18 16:55:32 +0900

    Feat: Add image api on Component

--- src/api/domain/image.api.ts
+++ src/api/domain/image.api.ts
@@ -1,4 +1,4 @@
-import { useMutation, UseMutationOptions, useQuery, UseQueryOptions } from '@tanstack/react-query';
+import { useMutation, UseMutationOptions } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
 
 import privateApi from '~/api/config/privateApi';
