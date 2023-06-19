ed5ba0c46a455756567c6f4e3572a9910f539f43
yunho <kimyouknow@naver.com>
2023-06-19 12:50:59 +0900

    Chore: TODO 주석추가

--- src/app/my-page/empty/page.tsx
+++ src/app/my-page/empty/page.tsx
@@ -1,5 +1,6 @@
 import { PlanetCreationButton } from '~/modules/PlanetCreationButton';
 
+// TODO: 임시 페이지, 디자인팀에게 요청한 상태, 홈화면의 소속 없는 경우 활용 가능성 있어요~
 const EmptyPlanet = () => {
   return (
     <main className="pt-35pxr">
