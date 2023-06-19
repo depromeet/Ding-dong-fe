f8c229d90c57ec043bad0f956c4d187f420cdd16
yunho <kimyouknow@naver.com>
2023-06-19 13:57:42 +0900

    Feat: 현재 활성화된 커뮤니티 id 가져오기

--- src/app/my-page/config/PlanetMenu.client.tsx
+++ src/app/my-page/config/PlanetMenu.client.tsx
@@ -1,9 +1,12 @@
 'use client';
 import { Menu } from '~/components/Menu';
+import { useCommunityStore } from '~/stores/community.store';
 
 export const PlanetMenu = () => {
+  const { communityId } = useCommunityStore();
+
   const onClickEscapePlanet = () => {
-    console.log('행성 떠나기 로직 추가 예정');
+    console.log('행성 떠나기 로직 추가 예정', communityId);
   };
   return (
     <Menu className="px-layout-sm">
