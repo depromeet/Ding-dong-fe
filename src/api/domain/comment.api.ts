af7cfafe214fa196df47192560dc55b95f9ec26d
yunho <kimyouknow@naver.com>
2023-06-21 01:19:00 +0900

    Feat: comment api (getComments, getCommentCounts)

---
+++ src/api/domain/comment.api.ts
@@ -0,0 +1,31 @@
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
+
+import privateApi from '~/api/config/privateApi';
+import { CommentCountGetRequest, CommentGetRequest, CommentGetResponse } from '~/types/comment';
+
+export const commentQueryKey = {
+  comments: (idCardsId: number, pageParam: number) => ['getComments', idCardsId, pageParam],
+  commentCount: (idCardsId: number) => ['getCommentCounts', idCardsId],
+};
+
+export const getComments = ({ idCardsId, pageParam }: CommentGetRequest) =>
+  privateApi.get<CommentGetResponse>(`/id-cards/${idCardsId}/comments?page=${pageParam}&size=10`);
+
+export const useGetComments = ({ idCardsId, pageParam }: CommentGetRequest) => {
+  return useInfiniteQuery(
+    commentQueryKey.comments(idCardsId, pageParam),
+    ({ pageParam = 0 }) => getComments({ idCardsId, pageParam }),
+    {
+      getNextPageParam: data => (!data.commentDto.hasNext ? data.commentDto.page + 1 : undefined),
+      refetchOnWindowFocus: false,
+      //NOTE: 서버컴포넌트에서 이미 1페이지를 데이터 fetch 했기 때문에 2페이지 부터 fetch 하기 위함입니다.
+      enabled: false,
+    },
+  );
+};
+
+export const getCommentCounts = ({ idCardsId }: CommentCountGetRequest) =>
+  privateApi.get<CommentGetResponse>(`/id-cards/${idCardsId}/comments-count`);
+
+export const useGetCommentCounts = ({ idCardsId }: CommentCountGetRequest) =>
+  useQuery(commentQueryKey.commentCount(idCardsId), () => getCommentCounts({ idCardsId }));
