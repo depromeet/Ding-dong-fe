2f5a7979ec6098583a926daeac85456dd7c536af
yunho <kimyouknow@naver.com>
2023-06-21 02:04:56 +0900

    Feat: commentMockHandler

---
+++ src/mocks/comment/comment.mock.ts
@@ -0,0 +1,48 @@
+import { faker } from '@faker-js/faker/locale/ko';
+
+import { SliceResponse } from '~/types/api';
+import { CommentModel, CommentReplyModel } from '~/types/comment';
+
+export const createCommentLike = () => ({
+  likeCount: faker.number.int({ min: 0, max: 999 }),
+  isLikedByCurrentUser: Math.random() > 0.5 ? true : false,
+});
+
+export const createCommentReply = (idx: number): CommentReplyModel => ({
+  commentReplyId: idx,
+  content: faker.lorem.paragraph(2),
+  createdAt: faker.date.betweens({
+    from: '2023-01-01T00:00:00.000Z',
+    to: '2023-08-31T00:00:00.000Z',
+  }) as unknown as string,
+  commentReplyLikeInfo: createCommentLike(),
+});
+
+export const createCommentReplyList = (n: number) =>
+  Array.from({ length: n }, (_, idx) => createCommentReply(Number(`${idx}${idx}`)));
+
+export const createComment = (idx: number): CommentModel => ({
+  commentId: idx,
+  content: faker.lorem.paragraph(2),
+  createdAt: faker.date.betweens({
+    from: '2023-01-01T00:00:00.000Z',
+    to: '2023-08-31T00:00:00.000Z',
+  }) as unknown as string,
+  commentReplyLikeInfo: createCommentLike(),
+  commentReplyInfos: createCommentReplyList(faker.number.int({ min: 0, max: 100 })),
+});
+
+export const createRandomCommentList = (
+  n: number,
+  page: number,
+  size: number,
+): SliceResponse<CommentModel> => ({
+  content: Array.from({ length: n }, (_, idx) => createComment(idx)),
+  page,
+  size,
+  hasNext: page === 5,
+});
+
+export const createCommentCount = () => ({
+  count: faker.number.int({ min: 0, max: 999 }),
+});
---
+++ src/mocks/comment/comment.mockHandler.ts
@@ -0,0 +1,20 @@
+import { rest } from 'msw';
+
+import { ROOT_API_URL } from '~/api/config/requestUrl';
+import { createCommentCount, createRandomCommentList } from '~/mocks/comment/comment.mock';
+
+export const commentMockHandler = [
+  rest.get(`${ROOT_API_URL}/id-cards/:idCardsId/comments?page=:page&size=10`, (req, res, ctx) => {
+    const { searchParams } = req.url;
+    const page = Number(searchParams.get('page'));
+    return res(
+      ctx.status(200),
+      ctx.json({
+        commentDto: createRandomCommentList(10, page, 10),
+      }),
+    );
+  }),
+  rest.get(`${ROOT_API_URL}/id-cards/:idCardsId/comments-count`, (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(createCommentCount()));
+  }),
+];
