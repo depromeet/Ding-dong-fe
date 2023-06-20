f52dc74922fb7f65b01cbdaee1c263ef88d3b885
99-zziy <wldud8229@gmail.com>
2023-06-21 00:31:37 +0900

    Feat: 짧은 소개글 스토리 추가

--- src/modules/CommunityDetail/CommunityDetail.stories.tsx
+++ src/modules/CommunityDetail/CommunityDetail.stories.tsx
@@ -16,7 +16,19 @@ export const Default: Story = {
       logoImageUrl="/assets/images/pipi.png"
       title="Depromeet"
       idCardCount={22}
-      description="디프만은 디자이너와 개발자가기획부터 론칭까지 함께 경험하는 성장추구형 커뮤니티입니다"
+      description="디프만은 디자이너와 개발자가 기획부터 론칭까지 함께 경험하는 성장추구형 커뮤니티입니다"
+    />
+  ),
+};
+
+export const ShortDescription: Story = {
+  render: () => (
+    <CommunityDetail
+      coverImageUrl="/assets/images/planet-with-shadow.png"
+      logoImageUrl="/assets/images/pipi.png"
+      title="Depromeet"
+      idCardCount={22}
+      description="디프만 입니다."
     />
   ),
 };
