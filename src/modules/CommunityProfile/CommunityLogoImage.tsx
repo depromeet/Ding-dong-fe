be38886016482627214a11135913b3bc753a91b4
darae07 <leedr0730@gmail.com>
2023-06-20 23:54:24 +0900

    feat: add logo size

--- src/modules/CommunityProfile/CommunityLogoImage.tsx
+++ src/modules/CommunityProfile/CommunityLogoImage.tsx
@@ -1,11 +1,21 @@
 import Image from 'next/image';
+import { twMerge } from 'tailwind-merge';
 
+type LogoSize = 'small' | 'medium' | 'large';
 type CommunityLogoImageProps = {
   logoImageUrl: string;
+  size?: LogoSize;
 };
-export const CommunityLogoImage = ({ logoImageUrl }: CommunityLogoImageProps) => {
+
+const sizes: Record<LogoSize, string> = {
+  small: 'h-20pxr w-20pxr',
+  medium: 'h-32pxr w-32pxr',
+  large: 'h-60pxr w-60pxr',
+};
+export const CommunityLogoImage = ({ logoImageUrl, size = 'large' }: CommunityLogoImageProps) => {
+  const logoSize = sizes[size];
   return (
-    <div className="h-60pxr w-60pxr flex-shrink-0 rounded-full">
+    <div className={twMerge(logoSize, 'flex-shrink-0 rounded-full')}>
       <Image width={60} height={60} src={logoImageUrl} alt="planet logo image" />
     </div>
   );
