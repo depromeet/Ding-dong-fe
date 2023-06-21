ca716e99682287a63a363013560ea2fbfd3af3b2
hyehyeon-moon <hyehyeon.moon@linecorp.com>
2023-06-22 00:52:41 +0900

    Fix: Use existed CharacterNameModel type

--- src/modules/Onboarding/CharacterCompleteStep.tsx
+++ src/modules/Onboarding/CharacterCompleteStep.tsx
@@ -1,44 +1,44 @@
 import Image from 'next/image';
 
 import { Template } from '~/components/Template';
+import { CharacterNameModel } from '~/types/idCard';
 
 type CharacterCompleteStepProps = {
-  characterId: Characters;
+  characterName: CharacterNameModel;
 };
 
-type Characters = 'buddy' | 'toby' | 'pipi' | 'true';
 type CharacterInfo = {
   title: string;
   description: string;
   image: string;
 };
-const characterInfo: Record<Characters, CharacterInfo> = {
-  buddy: {
+const characterInfo: Record<CharacterNameModel, CharacterInfo> = {
+  BUDDY: {
     title: '아이디어 뱅크',
     description: '도전정신이 가득한 버디와 함께하면\n 새롭고 재밌는 경험을 할 수 있을 거예요!',
     image: '/assets/images/character-buddy.png',
   },
-  toby: {
+  TOBBY: {
     title: '자유로운 영혼',
     description:
       '자유분방하고 여유 넘치는 토비는 넘치는 끼와\n 패션센스로 가끔 주민들의 이목을 집중시켜요.',
     image: '/assets/images/character-toby.png',
   },
-  pipi: {
+  PIPI: {
     title: '모두의 뮤즈',
     description:
       '감성적이고 사교적인 피피는 어느 행성을\n 가더라도 무리의 중심에서 주민들을 이끌어요.',
     image: '/assets/images/character-pipi.png',
   },
-  true: {
+  TRUE: {
     title: '현실주의자',
     description: '신중하고 믿음직한 트루는 한번 친해지면\n 누구보다도 든든한 친구가 되어줄 거예요.',
     image: '/assets/images/character-true.png',
   },
 };
 
-export const CharacterCompleteStep = ({ characterId }: CharacterCompleteStepProps) => {
-  const { title, description, image } = characterInfo[characterId];
+export const CharacterCompleteStep = ({ characterName }: CharacterCompleteStepProps) => {
+  const { title, description, image } = characterInfo[characterName];
 
   return (
     <Template>
