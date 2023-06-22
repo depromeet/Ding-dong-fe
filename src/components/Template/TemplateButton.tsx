663b1861d05c5d1ef0d6171d700c16f796b58d29
YUNHO <kimyouknow@naver.com>
2023-06-22 15:09:05 +0900

    Refactor: 경로수정 (#97)

--- src/components/Template/TemplateButton.tsx
+++ src/components/Template/TemplateButton.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from 'react';
 
-import Button from '~/components/Button/Button';
+import { Button } from '~/components/Button';
 import { tw } from '~/utils/tailwind.util';
 
 type TemplateButtonProps = {
