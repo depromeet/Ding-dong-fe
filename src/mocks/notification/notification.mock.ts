60f3829b84c3b3b69c8f3dbe6c571f1f096972fe
darae07 <leedr0730@gmail.com>
2023-06-24 00:26:27 +0900

    refactor: 날짜 오늘부터 이전으로

--- src/mocks/notification/notification.mock.ts
+++ src/mocks/notification/notification.mock.ts
@@ -21,21 +21,24 @@ export const createUser = (idx: number): NotificationModel['userDto'] => ({
   fromUserProfileImageUrl: faker.image.avatar(),
   fromUserNickname: faker.person.fullName(),
 });
-export const createNotification = (idx: number): NotificationModel => ({
-  notificationId: idx,
-  notificationType:
-    Math.random() > 0.7
-      ? 'COMMENT_LIKE'
-      : Math.random() > 0.5
-      ? 'COMMENT_REPLY'
-      : 'ID_CARD_COMMENT',
-  notificationStatus: Math.random() > 0.5 ? 'READ' : 'UNREAD',
-  createdAt: faker.date.anytime().toLocaleString(),
-  communityDto: createCommunity(idx),
-  idCardDto: createIdCard(idx),
-  commentDto: createComment(idx),
-  userDto: createUser(idx),
-});
+export const createNotification = (idx: number): NotificationModel => {
+  const today = new Date();
+  return {
+    notificationId: idx,
+    notificationType:
+      Math.random() > 0.7
+        ? 'COMMENT_LIKE'
+        : Math.random() > 0.5
+        ? 'COMMENT_REPLY'
+        : 'ID_CARD_COMMENT',
+    notificationStatus: Math.random() > 0.5 ? 'READ' : 'UNREAD',
+    createdAt: new Date(today.setDate(today.getDate() - idx)).toLocaleString(),
+    communityDto: createCommunity(idx),
+    idCardDto: createIdCard(idx),
+    commentDto: createComment(idx),
+    userDto: createUser(idx),
+  };
+};
 
 export const createNotificationList = (n: number, page: number, size: number) => ({
   content: Array.from({ length: n }, (_, idx) => createNotification(idx)),
