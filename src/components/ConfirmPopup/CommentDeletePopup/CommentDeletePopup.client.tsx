e5f9c4a8105463d69e872a96988884f1b1e1daa9
99-zziy <wldud8229@gmail.com>
2023-06-23 19:17:12 +0900

    Feat: CommentDeletePopup 추가

---
+++ src/components/ConfirmPopup/CommentDeletePopup/CommentDeletePopup.client.tsx
@@ -0,0 +1,31 @@
+'use client';
+import { Button } from '~/components/Button';
+import { TextButton } from '~/components/Button';
+import { ConfirmType } from '~/components/ConfirmPopup/useConfirmPopup';
+import Popup from '~/components/Popup/Popup';
+
+const TITLE = '댓글 삭제';
+const DESC = '댓글을 삭제할까요?';
+
+type ConfirmDeleteKeywordProps = {
+  confirm: (type: ConfirmType) => void;
+};
+
+export const CommentDeletePopup = ({ confirm }: ConfirmDeleteKeywordProps) => {
+  const buttons = (
+    <div className="flex w-full flex-col">
+      <Button onClick={() => confirm('OK')} type="button" size="medium" color="primary">
+        삭제
+      </Button>
+      <TextButton
+        onClick={() => confirm('CANCEL')}
+        type="button"
+        className="rounded-xl pt-13pxr text-b3 text-grey-900"
+      >
+        취소
+      </TextButton>
+    </div>
+  );
+
+  return <Popup title={TITLE} description={DESC} buttons={buttons} />;
+};
