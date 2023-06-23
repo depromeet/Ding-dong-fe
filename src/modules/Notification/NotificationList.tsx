adab6de0bb0718ca41494e52f2f5bdb3e8dd8801
darae07 <leedr0730@gmail.com>
2023-06-24 01:18:09 +0900

    feat: 날짜 분기 UI 추가

--- src/modules/Notification/NotificationList.tsx
+++ src/modules/Notification/NotificationList.tsx
@@ -1,4 +1,5 @@
 import { NotificationModel } from '~/types/notification';
+import { checkNotificationAgo } from '~/utils/notification';
 
 import { NotificationItem } from './NotificationItem';
 
@@ -6,10 +7,22 @@ type NotificationListProps = {
   notifications: NotificationModel[];
 };
 export const NotificationList = ({ notifications }: NotificationListProps) => {
+  const notificationAgoList = notifications.map(notification =>
+    checkNotificationAgo(notification.createdAt),
+  );
+
   return (
     <ul className="flex flex-col gap-7">
-      {notifications.map(notification => (
-        <NotificationItem {...notification} key={notification.notificationId} />
+      {notifications.map((notification, i) => (
+        <>
+          {(i === 0 || notificationAgoList[i] !== notificationAgoList[i - 1]) && (
+            <>
+              {i !== 0 && <div className="-ml-5 w-screen border-b border-b-gray-200"></div>}
+              <h2 className="text-h4">{notificationAgoList[i]}</h2>
+            </>
+          )}
+          <NotificationItem {...notification} key={notification.notificationId} />
+        </>
       ))}
     </ul>
   );
