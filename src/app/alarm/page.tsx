71fb58c42956d84dcd05c32bbbc410d5cfa0903d
darae07 <leedr0730@gmail.com>
2023-06-24 02:29:49 +0900

    feat: add prefetchQuery

--- src/app/alarm/page.tsx
+++ src/app/alarm/page.tsx
@@ -1,6 +1,17 @@
+import 'server-only';
+
+import { notificationQueryKey } from '~/api/domain/notification.api';
+import { getNotificationsServer } from '~/api/domain/notification.api.server';
+import getQueryClient from '~/lib/tanstackQuery/getQueryClient';
+
 import { InjectQueryDataNotification } from './InjectQueryDataNotification.client';
 
-const AlarmPage = () => {
+const AlarmPage = async () => {
+  const queryClient = getQueryClient();
+
+  await queryClient.prefetchQuery(notificationQueryKey.notifications(1), () =>
+    getNotificationsServer({ pageParam: 1 }).then(data => ({ pages: [data] })),
+  );
   return <InjectQueryDataNotification />;
 };
 
