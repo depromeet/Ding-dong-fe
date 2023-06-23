259799e0b8588c213fabc862061eb7e4b22f54fe
darae07 <leedr0730@gmail.com>
2023-06-23 23:01:13 +0900

    feat: add mock handler

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -2,6 +2,13 @@ import { commentMockHandler } from '~/mocks/comment/comment.mockHandler';
 import { communityMockHandler } from '~/mocks/community/community.mockHandler';
 import { idCardMockHandler } from '~/mocks/idCard/idCard.mockHandler';
 
-const handlers = [...idCardMockHandler, ...communityMockHandler, ...commentMockHandler];
+import { notificationMockHandler } from './notification/notification.mookHandler';
+
+const handlers = [
+  ...idCardMockHandler,
+  ...communityMockHandler,
+  ...commentMockHandler,
+  ...notificationMockHandler,
+];
 
 export default handlers;
