9027a3ad01d51426bd263116988cc78488fa14d2
darae07 <leedr0730@gmail.com>
2023-06-25 01:52:30 +0900

    feat: useGetUserId hook 추가

--- src/api/domain/user.api.ts
+++ src/api/domain/user.api.ts
@@ -2,6 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 
 import privateApi from '~/api/config/privateApi';
 import { UserInfoResponse } from '~/types/user';
+import { UserIdNotFoundError } from '~/utils/auth/error';
 
 export const userQueryKey = {
   userInfo: () => ['userInfo'],
@@ -10,3 +11,10 @@ export const userQueryKey = {
 export const getUserInfo = () => privateApi.get<UserInfoResponse>(`/user/profile`);
 
 export const useGetUserInfo = () => useQuery(userQueryKey.userInfo(), () => getUserInfo());
+
+export const useGetUserId = (): number => {
+  const { data } = useGetUserInfo();
+  const { userId } = data ?? {};
+  if (!userId) throw new UserIdNotFoundError();
+  return userId;
+};
--- src/utils/auth/getUserId.client.ts
+++
@@ -1,8 +0,0 @@
-import { UserIdNotFoundError } from './error';
-import { getAuthTokensByCookie } from './tokenHandlers';
-
-export const getUserIdClient = (): number => {
-  const { userId } = getAuthTokensByCookie(document.cookie);
-  if (userId !== undefined) return userId;
-  throw new UserIdNotFoundError();
-};
