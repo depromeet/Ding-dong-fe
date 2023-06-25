d33100c8f1361405d56db0a6bcc7fcb0a8f5c819
darae07 <leedr0730@gmail.com>
2023-06-26 00:08:33 +0900

    feat: HydrationProvider 적용

--- src/app/alarm/page.tsx
+++ src/app/alarm/page.tsx
@@ -2,17 +2,29 @@ import 'server-only';
 
 import { notificationQueryKey } from '~/api/domain/notification.api';
 import { getNotificationsServer } from '~/api/domain/notification.api.server';
-import getQueryClient from '~/lib/tanstackQuery/getQueryClient';
+import { HydrationProvider } from '~/components/HydrationProvider';
 
 import { InjectQueryDataNotification } from './InjectQueryDataNotification.client';
 
 const AlarmPage = async () => {
-  const queryClient = getQueryClient();
+  const getNotificationsQuery = async () => {
+    const data = await getNotificationsServer({ pageParam: 1 });
+    return {
+      pages: [data],
+    };
+  };
 
-  await queryClient.prefetchQuery(notificationQueryKey.notifications(1), () =>
-    getNotificationsServer({ pageParam: 1 }).then(data => ({ pages: [data] })),
+  return (
+    <>
+      {/* @ts-expect-error Server Component */}
+      <HydrationProvider
+        queryKey={notificationQueryKey.notifications(1)}
+        queryFn={getNotificationsQuery}
+      >
+        <InjectQueryDataNotification />
+      </HydrationProvider>
+    </>
   );
-  return <InjectQueryDataNotification />;
 };
 
 export default AlarmPage;
