3dbaae97db7a58c7177bcf51d4aa399bdf268c1c
darae07 <leedr0730@gmail.com>
2023-06-25 13:36:14 +0900

    feat: add server side guard

--- src/utils/auth/getUserId.client.ts
+++ src/utils/auth/getUserId.client.ts
@@ -2,7 +2,12 @@ import { UserIdNotFoundError } from './error';
 import { getAuthTokensByCookie } from './tokenHandlers';
 
 export const getUserIdClient = (): number => {
-  const { userId } = getAuthTokensByCookie(document.cookie);
-  if (userId !== undefined) return userId;
-  throw new UserIdNotFoundError();
+  try {
+    if (typeof document === 'undefined') throw new UserIdNotFoundError();
+    const { userId } = getAuthTokensByCookie(document.cookie);
+    if (userId !== undefined) return userId;
+    throw new UserIdNotFoundError();
+  } catch (e) {
+    return -1;
+  }
 };
