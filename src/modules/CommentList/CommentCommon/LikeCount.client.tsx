b81ae8f9d639e249e8d458b20ad773d4afa7b49b
yunho <kimyouknow@naver.com>
2023-06-26 23:30:48 +0900

    Refactor: 좋아요 0 개일 때 UI 수정

--- src/modules/CommentList/CommentCommon/LikeCount.client.tsx
+++ src/modules/CommentList/CommentCommon/LikeCount.client.tsx
@@ -3,5 +3,6 @@ import { CommentLikeModel } from '~/types/comment';
 type LikeCountProps = Pick<CommentLikeModel, 'likeCount'>;
 
 export const LikeCount = ({ likeCount }: LikeCountProps) => {
-  return <span className="text-detail text-grey-500">좋아요 {likeCount}개</span>;
+  const countMessage = likeCount === 0 ? '좋아요' : `좋아요 ${likeCount}개`;
+  return <span className="text-detail text-grey-500">{countMessage}</span>;
 };
