0f4586322589e3d80dc53669ec2f12e4f222a5c6
99-zziy <wldud8229@gmail.com>
2023-06-27 17:28:42 +0900

    Feat: ReplyIndicator 취소 버튼 추가

--- src/modules/CommentInput/ReplyIndicator.client.tsx
+++ src/modules/CommentInput/ReplyIndicator.client.tsx
@@ -3,15 +3,17 @@
 import React from 'react';
 
 import { Divider } from '~/components/Divider';
+import { CancelIcon } from '~/components/Icon';
 import { useReplyRecipientStore } from '~/stores/comment.store';
 
 export const ReplyIndicator = () => {
-  const { nickname } = useReplyRecipientStore();
+  const { nickname, clear } = useReplyRecipientStore();
   return nickname ? (
     <>
       <Divider className="bg-grey-200" />
-      <div className="bg-grey-200 px-20pxr py-10pxr text-b3 text-grey-500">
+      <div className="flex justify-between bg-grey-200 px-20pxr py-10pxr text-b3 text-grey-500">
         <span>{nickname}님에게 답글 남기는 중</span>
+        <CancelIcon className="p-3pxr" onClick={() => clear()} />
       </div>
     </>
   ) : (
