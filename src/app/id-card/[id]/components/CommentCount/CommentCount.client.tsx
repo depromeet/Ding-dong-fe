82f3dd7ff9aad67df92d0407164d3282100da0fc
99-zziy <wldud8229@gmail.com>
2023-06-30 22:48:38 +0900

    Feat: 클라이언트 컴포넌트로 수정

--- src/app/id-card/[id]/components/CommentCount/CommentCount.client.tsx
+++ src/app/id-card/[id]/components/CommentCount/CommentCount.client.tsx
@@ -1,18 +1,20 @@
+'use client';
 import { Suspense } from 'react';
 
-import { getCommentCountsServer } from '~/api/domain/comment.api.server';
+import { useGetCommentCounts } from '~/api/domain/comment.api';
 import RetryErrorBoundary from '~/components/ErrorBoundary/RetryErrorBoundary.client';
 
 type CommentCountProps = {
   idCardId: number;
 };
 
-const CommentCountComponent = async ({ idCardId }: CommentCountProps) => {
-  const totalCommentCount = await getCommentCountsServer({ idCardId });
+const CommentCountComponent = ({ idCardId }: CommentCountProps) => {
+  const { data: totalCommentCount } = useGetCommentCounts({ idCardId });
+  console.log('totalCommentCount', totalCommentCount);
 
   return (
     <div className="mt-24pxr px-layout-sm text-b2 text-grey-900">
-      <span>댓글 {totalCommentCount.count}개</span>
+      {totalCommentCount && <span>댓글 {totalCommentCount.count}개</span>}
     </div>
   );
 };
@@ -21,7 +23,6 @@ export const CommentCount = ({ idCardId }: CommentCountProps) => {
   return (
     <RetryErrorBoundary>
       <Suspense>
-        {/* @ts-expect-error Server Component */}
         <CommentCountComponent idCardId={idCardId} />
       </Suspense>
     </RetryErrorBoundary>
