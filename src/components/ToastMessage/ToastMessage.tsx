aaccce9c2cdbd4c2cc684abb811b54c45385a9e1
yunho <kimyouknow@naver.com>
2023-06-30 23:27:46 +0900

    refactor: toast message css

--- src/components/ToastMessage/ToastMessage.tsx
+++ src/components/ToastMessage/ToastMessage.tsx
@@ -4,9 +4,9 @@ import { tw } from '~/utils/tailwind.util';
 type ToastMessageProps = Omit<ToastMessageModel, 'toastId'>;
 
 const colors: Record<ToastMessageType, string> = {
-  error: 'bg-grey-700 text-white',
-  success: 'bg-grey-700 text-white',
-  info: 'bg-grey-700 text-white',
+  error: 'bg-grey-500 text-white',
+  success: 'bg-grey-500 text-white',
+  info: 'bg-grey-500 text-white',
 };
 
 export const ToastMessage = ({ message, type }: ToastMessageProps) => {
