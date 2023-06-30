36b34d5e7ee3554a0cbe02b51c878a64d9ef0209
darae07 <leedr0730@gmail.com>
2023-07-01 03:11:04 +0900

    feat: 알림 sse 구독

--- src/modules/Notification/NewNotificationBadge.client.tsx
+++ src/modules/Notification/NewNotificationBadge.client.tsx
@@ -1,7 +1,29 @@
-import { useGetUnreadNotification } from '~/api/domain/notification.api';
+import { useQueryClient } from '@tanstack/react-query';
+import { usePathname } from 'next/navigation';
+import { useEffect } from 'react';
+
+import { ROOT_API_URL } from '~/api/config/requestUrl';
+import { notificationQueryKey, useGetUnreadNotification } from '~/api/domain/notification.api';
 
 export const NewNotificationBadge = () => {
   const hasUnreadNotification = useGetUnreadNotification();
+
+  const queryClient = useQueryClient();
+  const pathname = usePathname();
+
+  useEffect(() => {
+    const newNotificationSource = new EventSource(`${ROOT_API_URL}/notifications/subscribe`);
+
+    newNotificationSource.addEventListener('sse', () => {
+      queryClient.setQueryData(notificationQueryKey.unread(), { data: true });
+    });
+  }, [queryClient]);
+
+  useEffect(() => {
+    if (pathname.includes('/notification')) {
+      queryClient.setQueryData(notificationQueryKey.unread(), { data: false });
+    }
+  }, [pathname, queryClient]);
 
   if (hasUnreadNotification.data?.data) {
     return (
