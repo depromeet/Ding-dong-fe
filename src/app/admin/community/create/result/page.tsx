b6bae5206fe2f1c7551eceea7a6e836f645c1bce
yunho <kimyouknow@naver.com>
2023-07-01 23:38:33 +0900

    Feat: 행성 생성 완료에 팝업 적용

--- src/app/admin/community/create/result/page.tsx
+++ src/app/admin/community/create/result/page.tsx
@@ -1,9 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 'use client';
 
 import { useSearchParams } from 'next/navigation';
 
 import { useGetCommunityDetail } from '~/api/domain/community.api';
 import { Button } from '~/components/Button';
+import { CopyInvitation, useConfirmPopup } from '~/components/ConfirmPopup';
 import { KakaoIcon } from '~/components/Icon/KakaoIcon';
 
 const AdminCommunityCreateResultPage = () => {
@@ -11,11 +13,42 @@ const AdminCommunityCreateResultPage = () => {
   const communityIdParam = searchParams.get('communityId');
   const communityId = isNaN(Number(communityIdParam)) ? -1 : Number(communityIdParam);
   const { data } = useGetCommunityDetail(communityId);
-  console.log(data);
+
+  const {
+    isOpen: isCopyInvitationOpen,
+    openPopup: openCopyInvitationPopup,
+    closePopup: closeCopyInvitationPopup,
+    confirm: copyInvitation,
+  } = useConfirmPopup();
+
+  const {
+    isOpen: isKakaoShareOpen,
+    openPopup: openKakaoSharePopup,
+    closePopup: closeKakaoSharePopup,
+    confirm: kakaoShare,
+  } = useConfirmPopup();
+
+  const onClickCopyWebLinkButton = async () => {
+    const isOk = await openCopyInvitationPopup();
+    closeCopyInvitationPopup();
+    if (isOk) {
+      // router.back();
+    }
+    return;
+  };
+
+  const onClickKakaoShareButton = async () => {
+    const isOk = await openKakaoSharePopup();
+    closeKakaoSharePopup();
+    if (isOk) {
+      // router.back();
+    }
+    return;
+  };
 
   return (
     <div className="mt-3pxr flex flex-col gap-16pxr">
-      <Button color="primary" size="xLarge">
+      <Button color="primary" size="xLarge" onClick={onClickCopyWebLinkButton}>
         초대 링크 복사하기
       </Button>
       <Button
@@ -23,9 +56,11 @@ const AdminCommunityCreateResultPage = () => {
         size="medium"
         className="flex justify-center gap-4pxr bg-[#F9DF4A] pb-15pxr pt-17pxr text-[#391B1B]"
       >
-        <KakaoIcon className="mt-1pxr" />
+        <KakaoIcon className="mt-1pxr" onClick={onClickKakaoShareButton} />
         카카오톡으로 초대하기
       </Button>
+      {isCopyInvitationOpen && <CopyInvitation confirm={copyInvitation} />}
+      {isKakaoShareOpen && <CopyInvitation confirm={kakaoShare} />}
     </div>
   );
 };
