4ab3e47069596f7019a6706a838fff61eade34b8
hyehyeon-moon <hyehyeon.moon@linecorp.com>
2023-07-01 11:05:31 +0900

    Feat: Move main routing logic from middleware to client component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,31 @@
+`use client`;
+
+import { useRouter } from 'next/navigation';
+
+import { useGetUserInfo } from '~/api/domain/user.api';
 import PlanetPage from '~/app/planet/page';
+import { getUserIdClient } from '~/utils/auth/getUserId.client';
 
 const Home = () => {
+  const router = useRouter();
+  const userId = getUserIdClient();
+  const { data: userInfo } = useGetUserInfo({
+    enabled: !!userId,
+  });
+
+  if (userId) {
+    if (userInfo) {
+      const { isCharacterCreated, planetIds } = userInfo;
+      if (isCharacterCreated) {
+        planetIds.length > 0 ? router.push(`/planet/${planetIds[0]}`) : router.push('/planet');
+      } else {
+        router.push('/onboarding');
+      }
+    }
+  } else {
+    router.push('/auth/signin');
+  }
+
   return (
     <main>
       <PlanetPage />
