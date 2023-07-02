bee7a962ec18ad9eccebb6913c85224740a389c6
YUNHO <kimyouknow@naver.com>
2023-07-02 23:22:34 +0900

    마이페이지 관련 수정 (버튼 동작 및 ui 수정) (#185)
    * feat: 마이페이지에서 행성 만들기 버튼 동작 수정
    
    * refactor: icon에 fill 제거
    
    * refactor: 주민증없는 경우 ui 수정

--- src/app/my-page/empty/page.tsx
+++ src/app/my-page/empty/page.tsx
@@ -1,10 +1,35 @@
-import { PlanetCreationButton } from '~/modules/PlanetCreationButton';
+'use client';
+import { useRouter } from 'next/navigation';
 
-// TODO: 임시 페이지, 디자인팀에게 요청한 상태, 홈화면의 소속 없는 경우 활용 가능성 있어요~
+import { PlusIcon } from '~/components/Icon';
+import { PlanetCreationButton } from '~/modules/PlanetCreationButton';
+import { useCommunityStore } from '~/stores/community.store';
+
 const EmptyPlanet = () => {
+  const { communityId } = useCommunityStore();
+  const router = useRouter();
+
+  // TODO: communityId가 없는 경우 에러 처리가 필요해요( ex. 홈이나 다른 페이지로 보내기)
+  const onClickCreateIdCardButton = () => {
+    router.push(`/planet/${communityId}/id-card/create`);
+  };
+
   return (
     <main className="pt-35pxr">
-      <div className="mx-layout-l">소속된 커뮤니티니가 없어요</div>
+      <div className="mx-layout-l">
+        <h1 className="text-h3 text-grey-800">내 주민증</h1>
+        <div className="mt-16pxr flex h-[404px] w-full items-center justify-center rounded-[16px] border border-dashed border-primary-500 bg-blue-100">
+          <div className="flex flex-col items-center gap-8pxr">
+            <button
+              onClick={onClickCreateIdCardButton}
+              className="flex h-[52px] w-[52px] items-center justify-center rounded-full bg-blue-200"
+            >
+              <PlusIcon className="fill-primary-500 stroke-2" />
+            </button>
+            <span className="text-b1 text-primary-500">주민증 만들기</span>
+          </div>
+        </div>
+      </div>
       <div className="mx-layout-sm mt-28pxr">
         <PlanetCreationButton />
       </div>
