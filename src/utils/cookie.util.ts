57a140c66e25d374f95e81badd30e8a2dc7f8dd4
hyehyeon-moon <hyehyeon.moon@linecorp.com>
2023-07-02 14:10:11 +0900

    Feat: Add cookie util

---
+++ src/utils/cookie.util.ts
@@ -0,0 +1,33 @@
+// Ref : https://ko.javascript.info/cookie
+
+export const getCookie = (name: string) => {
+  const matches = document.cookie.match(
+    // eslint-disable-next-line no-useless-escape
+    new RegExp('(?:^|; )' + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + '=([^;]*)'),
+  );
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const setCookie = (name: string, value: any, options?: any) => {
+  const cookieOptions = {
+    ...options,
+  };
+
+  let updatedCookie = encodeURIComponent(name) + '=' + encodeURIComponent(value);
+
+  for (const optionKey in cookieOptions) {
+    updatedCookie += '; ' + optionKey;
+    const optionValue = cookieOptions[optionKey];
+    if (optionValue !== true) {
+      updatedCookie += '=' + optionValue;
+    }
+  }
+  document.cookie = updatedCookie;
+};
+
+export const deleteCookie = (name: string) => {
+  setCookie(name, '', {
+    'max-age': -1,
+  });
+};
