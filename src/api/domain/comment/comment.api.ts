b206b64416b65609c982bebbee298441a57b54c4
99-zziy <wldud8229@gmail.com>
2023-07-02 15:58:51 +0900

    Fix: hasnext 수정

--- src/api/domain/comment/comment.api.ts
+++ src/api/domain/comment/comment.api.ts
@@ -60,7 +60,7 @@ export const useGetComments = ({ idCardId, pageParam }: CommentGetRequest) => {
     commentQueryKey.comments(idCardId),
     ({ pageParam = 0 }) => getComments({ idCardId, pageParam }),
     {
-      getNextPageParam: data => (!data.hasNext ? data.page + 1 : undefined),
+      getNextPageParam: data => (data.hasNext ? data.page + 1 : undefined),
       refetchOnWindowFocus: false,
       //NOTE: 서버컴포넌트에서 이미 1페이지를 데이터 fetch 했기 때문에 2페이지 부터 fetch 하기 위함입니다.
       enabled: false,
