56a33433b7ced4931db10b327153a59f21b840a7
darae07 <leedr0730@gmail.com>
2023-07-03 01:11:38 +0900

    feat: 행성 꾸미기 페이지

--- src/app/admin/community/[id]/edit/page.tsx
+++ src/app/admin/community/[id]/edit/page.tsx
@@ -1,5 +1,18 @@
-const AdminCommunityDetailPage = () => {
-  return <></>;
+'use client';
+
+import { useGetCommunityDetail } from '~/api/domain/community.api';
+import { CommunityAdminEdit } from '~/modules/CommunityAdmin/CommunityAdminEdit.client';
+
+type AdminCommunityDetailPageProps = {
+  params: {
+    id: number;
+  };
+};
+
+const AdminCommunityDetailPage = ({ params: { id } }: AdminCommunityDetailPageProps) => {
+  const { data } = useGetCommunityDetail(id);
+
+  return <>{data?.communityDetailsDto && <CommunityAdminEdit {...data.communityDetailsDto} />}</>;
 };
 
 export default AdminCommunityDetailPage;
