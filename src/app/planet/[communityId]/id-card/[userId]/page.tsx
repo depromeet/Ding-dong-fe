e968b1069cf0cb0d630e926d61e63ea43df5fe42
문혜현 <55529617+hyehyeonmoon@users.noreply.github.com>
2023-07-02 16:05:00 +0900

    Move id-card folder under planet folder (#165)
    Co-authored-by: hyehyeon-moon <hyehyeon.moon@linecorp.com>

--- src/app/planet/[communityId]/id-card/[userId]/page.tsx
+++ src/app/planet/[communityId]/id-card/[userId]/page.tsx
@@ -2,21 +2,21 @@ import 'server-only';
 
 import { commentQueryKey } from '~/api/domain/comment/comment.api';
 import { getCommentsServer } from '~/api/domain/comment/comment.api.server';
-import { CommentCount } from '~/app/id-card/[id]/components/CommentCount';
-import { CommentList } from '~/app/id-card/[id]/components/CommentList';
-import { IdCardDetail } from '~/app/id-card/[id]/components/IdCardDetail/IdCardDetail';
+import { CommentCount } from '~/app/planet/[communityId]/id-card/[userId]/components/CommentCount';
+import { CommentList } from '~/app/planet/[communityId]/id-card/[userId]/components/CommentList';
+import { IdCardDetail } from '~/app/planet/[communityId]/id-card/[userId]/components/IdCardDetail/IdCardDetail';
 import { Divider } from '~/components/Divider';
 import { HydrationProvider } from '~/components/HydrationProvider';
 import { CommentInput } from '~/modules/CommentInput';
 
 type IdCardDetailPageProps = {
   params: {
-    id: string;
+    userId: string;
   };
 };
 
-const IdCardDetailPage = async ({ params: { id } }: IdCardDetailPageProps) => {
-  const idCardId = Number(id);
+const IdCardDetailPage = async ({ params: { userId } }: IdCardDetailPageProps) => {
+  const idCardId = Number(userId);
   const pageParam = 1;
 
   const getCommentsQuery = async () => {
--- src/app/planet/[communityId]/page.tsx
+++ src/app/planet/[communityId]/page.tsx
@@ -2,19 +2,19 @@ import 'server-only';
 
 import { communityQueryKey } from '~/api/domain/community.api';
 import { getCommunityIdCardsServer } from '~/api/domain/community.api.server';
-import { CommunityDetail } from '~/app/planet/[id]/components/CommunityDetail';
-import { CommunityIdCards } from '~/app/planet/[id]/components/CommunityIdCards';
-import { IdCardCreatorButton } from '~/app/planet/[id]/components/IdCardCreatorButton';
+import { CommunityDetail } from '~/app/planet/[communityId]/components/CommunityDetail';
+import { CommunityIdCards } from '~/app/planet/[communityId]/components/CommunityIdCards';
+import { IdCardCreatorButton } from '~/app/planet/[communityId]/components/IdCardCreatorButton';
 import { HydrationProvider } from '~/components/HydrationProvider';
 
 type PlanetPageProps = {
   params: {
-    id: string;
+    communityId: string;
   };
 };
 
-const PlanetPage = async ({ params: { id } }: PlanetPageProps) => {
-  const communityId = Number(id);
+const PlanetPage = async ({ params: { communityId: communityIdParam } }: PlanetPageProps) => {
+  const communityId = Number(communityIdParam);
 
   const getCommunityIdCardsQuery = async () => {
     const data = await getCommunityIdCardsServer({ communityId });
