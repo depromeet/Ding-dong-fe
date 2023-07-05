24896eabde027d5078f985681b80b76db716df9b
강지영 <62633444+99-zziy@users.noreply.github.com>
2023-07-05 21:51:44 +0900

    Fix/no scroll login (#235)
    * Fix: login scroll #222
    
    * Fix: 애플로그인 주석 처리

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -2,7 +2,7 @@ import { LoginStep } from '~/modules/LoginStep';
 
 const SignInPage = () => {
   return (
-    <main className="px-layout-sm pt-t-nav">
+    <main className="flex h-[calc(100vh-60px)] items-center px-layout-sm pb-10pxr">
       <LoginStep />
     </main>
   );
