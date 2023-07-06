d3407b2ba59c80a384c78b1398b0c17908cb69ee
darae07 <leedr0730@gmail.com>
2023-07-07 01:11:04 +0900

    feat: 행성 떠나기 컨펌

--- src/modules/PlanetMenu/PlanetMenu.client.tsx
+++ src/modules/PlanetMenu/PlanetMenu.client.tsx
@@ -1,7 +1,9 @@
 'use client';
 import { useSearchParams } from 'next/navigation';
 
-import { useWithdrawalCommunity } from '~/api/domain/community.api';
+import { useGetCommunityDetail, useWithdrawalCommunity } from '~/api/domain/community.api';
+import { useConfirmPopup } from '~/components/ConfirmPopup';
+import { SimpleConfirmPopup } from '~/components/ConfirmPopup/SimpleConfirmPopup/SimpleConfirmPopup.client';
 import { Menu } from '~/components/Menu';
 
 export const PlanetMenu = () => {
@@ -9,16 +11,35 @@ export const PlanetMenu = () => {
   const communityIdParam = searchParams.get('communityId');
   const communityId = isNaN(Number(communityIdParam)) ? -1 : Number(communityIdParam);
   const { mutate } = useWithdrawalCommunity(communityId);
+  const { data: communityDetail } = useGetCommunityDetail(communityId);
 
-  const onClickEscapePlanet = () => {
-    mutate();
+  const onClickEscapePlanet = async () => {
+    const isOk = await openPopup();
+    closePopup();
+    if (isOk) {
+      mutate();
+    }
   };
+
+  const { isOpen, openPopup, closePopup, confirm } = useConfirmPopup();
+
   return (
-    <Menu className="px-layout-sm">
-      <Menu.Header>행성 관리</Menu.Header>
-      <Menu.Element onClick={onClickEscapePlanet}>
-        <span>행성 떠나기</span>
-      </Menu.Element>
-    </Menu>
+    <>
+      <Menu className="px-layout-sm">
+        <Menu.Header>행성 관리</Menu.Header>
+        <Menu.Element onClick={onClickEscapePlanet}>
+          <span>행성 떠나기</span>
+        </Menu.Element>
+      </Menu>
+      {isOpen && (
+        <SimpleConfirmPopup
+          confirm={confirm}
+          title="행성 탈퇴"
+          description={`${communityDetail?.communityDetailsDto.title} 정말로 떠나시겠습니까?`}
+          confirmText="행성 탈퇴"
+          cancelText="취소"
+        />
+      )}
+    </>
   );
 };
