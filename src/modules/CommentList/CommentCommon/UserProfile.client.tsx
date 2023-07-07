e829f1e6514a0f4aac5ba14e34bd927d56de5abc
문혜현 <55529617+hyehyeonmoon@users.noreply.github.com>
2023-07-08 00:11:59 +0900

    Fix: Add object-cover css on profile user image (#283)
    Co-authored-by: hyehyeon-moon <hyehyeon.moon@linecorp.com>

--- src/modules/CommentList/CommentCommon/UserProfile.client.tsx
+++ src/modules/CommentList/CommentCommon/UserProfile.client.tsx
@@ -7,7 +7,13 @@ type UserProfileProps = Pick<CommentWriterIntoModel, 'profileImageUrl'>;
 export const UserProfile = ({ profileImageUrl }: UserProfileProps) => {
   return (
     <div className="h-32pxr w-32pxr flex-shrink-0 overflow-hidden rounded-full">
-      <Image width={32} height={32} src={profileImageUrl} alt="profile image" />
+      <Image
+        width={32}
+        height={32}
+        src={profileImageUrl}
+        className="object-cover"
+        alt="profile image"
+      />
     </div>
   );
 };
