373435d35048f2152a979b2aa3a08c80bd1d7e1d
darae07 <leedr0730@gmail.com>
2023-07-08 00:50:38 +0900

    feat: 미사용 컴포넌트 삭제

--- src/app/api/auth/signin/signInProviders.server.tsx
+++ src/app/api/auth/signin/signInProviders.server.tsx
@@ -2,20 +2,10 @@ import 'server-only';
 
 import { getProviders } from 'next-auth/react';
 
-import Kakao from './kakaoLoginButton.client';
-
 const SignInProviders = async () => {
   const providers = await getProviders();
   if (!providers) return <></>;
-  return (
-    <div>
-      {Object.values(providers).map(provider => {
-        if (provider.id === 'kakao') {
-          return <Kakao provider={provider} key={provider.id} />;
-        }
-      })}
-    </div>
-  );
+  return <div></div>;
 };
 
 export { SignInProviders };
