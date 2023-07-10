628014e290a35034fb9b08d6a17a6276032ffd78
darae07 <leedr0730@gmail.com>
2023-07-11 00:29:09 +0900

    fix: redirect

--- src/app/my-page/route.ts
+++ src/app/my-page/route.ts
@@ -1,27 +1,21 @@
 import { cookies } from 'next/headers';
-import { NextRequest, NextResponse } from 'next/server';
+import { redirect } from 'next/navigation';
 
 import { privateApi } from '~/api/config/privateApi.server';
 import { AUTH_COOKIE_KEYS } from '~/types/auth';
 import { UserInfoResponse } from '~/types/user';
 
-export async function GET(request: NextRequest) {
-  try {
-    const cookieStore = cookies();
-    const accessToken = cookieStore.get(AUTH_COOKIE_KEYS.accessToken)?.value;
-    if (accessToken) {
-      const { userProfileDto } = await privateApi.get<UserInfoResponse>(`/user/profile`);
-      const { communityIds } = userProfileDto;
+export async function GET() {
+  const cookieStore = cookies();
+  const accessToken = cookieStore.get(AUTH_COOKIE_KEYS.accessToken)?.value;
+  if (accessToken) {
+    const { userProfileDto } = await privateApi.get<UserInfoResponse>(`/user/profile`);
+    const { communityIds } = userProfileDto;
 
-      if (communityIds.length !== 0) {
-        return NextResponse.redirect(
-          `${request.nextUrl.origin}/my-page/${communityIds[communityIds.length - 1]}`,
-        );
-      }
-      return NextResponse.redirect(`${request.nextUrl.origin}/my-page/empty`);
+    if (communityIds.length !== 0) {
+      redirect(`/my-page/${communityIds[communityIds.length - 1]}`);
     }
-    return NextResponse.redirect(`${request.nextUrl.origin}/auth/signin`);
-  } catch (e) {
-    return NextResponse.redirect(`${request.nextUrl.origin}/auth/signin`);
+    redirect(`/my-page/empty`);
   }
+  redirect(`/auth/signin`);
 }
