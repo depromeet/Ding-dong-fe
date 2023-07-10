4b838f0b7bf1ccc4b9fb6c659929af06624a555e
99-zziy <wldud8229@gmail.com>
2023-07-11 08:21:39 +0900

    Fix: max width 변경

--- src/app/planet/[communityId]/components/CommunityDetail/CommunityDetail.tsx
+++ src/app/planet/[communityId]/components/CommunityDetail/CommunityDetail.tsx
@@ -19,7 +19,7 @@ export const CommunityDetailComponent = async ({ id }: CommunityDetailProps) => 
   return (
     <div>
       {/*  width = 100vw, height = width * 0.48 */}
-      <div className="relative h-[calc(410px*0.48)] mobile:h-[calc(100vw*0.48)]">
+      <div className="relative h-[calc(420px*0.48)] mobile:h-[calc(100vw*0.48)]">
         <Image
           fill
           src={coverImageUrl ?? '/assets/images/planet-cover-default-image.png'}
@@ -27,7 +27,7 @@ export const CommunityDetailComponent = async ({ id }: CommunityDetailProps) => 
           style={{ objectFit: 'cover' }}
         />
       </div>
-      <div className="flex w-[calc(100vw-40px)] max-w-[calc(410px-40px)] translate-x-[20px] translate-y-[-50%] items-center gap-12pxr rounded-3xl border border-grey-100 bg-white p-18pxr">
+      <div className="flex w-[calc(100vw-40px)] max-w-[calc(420px-40px)] translate-x-[20px] translate-y-[-50%] items-center gap-12pxr rounded-3xl border border-grey-100 bg-white p-18pxr">
         <CommunityLogoImage logoImageUrl={logoImageUrl} />
         <div className="flex flex-col gap-8pxr">
           <p className="text-sm font-medium text-gray-800">{`주민 ${userCount ?? 0}`}</p>
