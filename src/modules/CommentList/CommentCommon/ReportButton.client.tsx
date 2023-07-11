daa5d06ceceb2b90a4e5292c3c19a5dd26a9588e
yunho <kimyouknow@naver.com>
2023-07-11 20:53:45 +0900

    feat: 댓글 신고 팝업 및 토스트 메시지

--- src/modules/CommentList/CommentCommon/ReportButton.client.tsx
+++ src/modules/CommentList/CommentCommon/ReportButton.client.tsx
@@ -1,6 +1,16 @@
+import { useConfirmPopup } from '~/components/ConfirmPopup';
+import { SimpleConfirmPopup } from '~/components/ConfirmPopup/SimpleConfirmPopup/SimpleConfirmPopup.client';
+import { useToastMessageStore } from '~/stores/toastMessage.store';
+
 export const ReportButton = () => {
+  const { isOpen, openPopup, closePopup, confirm } = useConfirmPopup();
+  const { infoToast } = useToastMessageStore();
   const handleReport = async () => {
-    // TODO: 신고 로직 추가하기
+    const isOk = await openPopup();
+    closePopup();
+    if (isOk) {
+      infoToast('정상적으로 신고됐어요!');
+    }
   };
 
   return (
@@ -8,6 +18,15 @@ export const ReportButton = () => {
       <button className="text-detail text-grey-400" onClick={() => handleReport()}>
         신고
       </button>
+      {isOpen && (
+        <SimpleConfirmPopup
+          confirm={confirm}
+          title="댓글 신고"
+          description="댓글을 신고할까요?"
+          confirmText="신고"
+          cancelText="취소"
+        />
+      )}
     </>
   );
 };
