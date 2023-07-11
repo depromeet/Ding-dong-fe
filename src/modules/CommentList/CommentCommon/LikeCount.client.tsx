7bceebe59673a163b4841327b2c69e9f89dcb091
yunho <kimyouknow@naver.com>
2023-07-11 15:41:15 +0900

    refactor: 좋아요 있을 때만 보여지도록 수정

--- src/modules/CommentList/CommentCommon/LikeCount.client.tsx
+++ src/modules/CommentList/CommentCommon/LikeCount.client.tsx
@@ -3,6 +3,8 @@ import { CommentLikeModel } from '~/types/comment';
 type LikeCountProps = Pick<CommentLikeModel, 'likeCount'>;
 
 export const LikeCount = ({ likeCount }: LikeCountProps) => {
-  const countMessage = likeCount === 0 ? '좋아요' : `좋아요 ${likeCount}개`;
-  return <span className="text-detail text-grey-500">{countMessage}</span>;
+  const isShowLikeText = likeCount !== 0;
+  return (
+    <>{isShowLikeText && <span className="text-detail text-grey-500">좋아요 {likeCount}개</span>}</>
+  );
 };
