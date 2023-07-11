92c34da6250b470f636575df325a93a1fd417f10
yunho <kimyouknow@naver.com>
2023-07-11 21:39:52 +0900

    refactor: comment LikeCount font

--- src/modules/CommentList/CommentCommon/LikeCount.client.tsx
+++ src/modules/CommentList/CommentCommon/LikeCount.client.tsx
@@ -5,6 +5,10 @@ type LikeCountProps = Pick<CommentLikeModel, 'likeCount'>;
 export const LikeCount = ({ likeCount }: LikeCountProps) => {
   const isShowLikeText = likeCount !== 0;
   return (
-    <>{isShowLikeText && <span className="text-detail text-grey-500">좋아요 {likeCount}개</span>}</>
+    <>
+      {isShowLikeText && (
+        <span className="text-detail font-medium text-grey-500">좋아요 {likeCount}개</span>
+      )}
+    </>
   );
 };
