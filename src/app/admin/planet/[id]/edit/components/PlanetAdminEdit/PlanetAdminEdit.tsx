553b3707e8e6f3681baa327a121cebab38318157
yunho <kimyouknow@naver.com>
2023-07-12 16:02:32 +0900

    refactor: PlanetAdminEdit Hydration 방식 수정
    서버 요청 없애기

--- src/app/admin/planet/[id]/edit/components/PlanetAdminEdit/PlanetAdminEdit.tsx
+++ src/app/admin/planet/[id]/edit/components/PlanetAdminEdit/PlanetAdminEdit.tsx
@@ -12,12 +12,11 @@ type PlanetAdminEditProps = {
   planetId: number;
 };
 
-const PlanetAdminEditComponent = async ({ planetId }: PlanetAdminEditProps) => {
+const PlanetAdminEditComponent = ({ planetId }: PlanetAdminEditProps) => {
   const getCommunityDetailQuery = async () => {
-    const { communityDetailsDto } = await getCommunityDetailServer(planetId);
-    return communityDetailsDto;
+    const data = await getCommunityDetailServer(planetId);
+    return data;
   };
-  const { communityDetailsDto } = await getCommunityDetailServer(planetId);
 
   return (
     <>
@@ -26,7 +25,7 @@ const PlanetAdminEditComponent = async ({ planetId }: PlanetAdminEditProps) => {
         queryKey={communityQueryKey.communityDetail(planetId)}
         queryFn={getCommunityDetailQuery}
       >
-        <CommunityAdminEdit {...communityDetailsDto} />
+        <CommunityAdminEdit communityId={planetId} />
       </HydrationProvider>
     </>
   );
@@ -36,7 +35,6 @@ export const PlanetAdminEdit = ({ planetId }: PlanetAdminEditProps) => {
   return (
     <RetryErrorBoundary>
       <Suspense>
-        {/* @ts-expect-error Server Component */}
         <PlanetAdminEditComponent planetId={planetId} />
       </Suspense>
     </RetryErrorBoundary>
