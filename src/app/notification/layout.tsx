fb2bd0d2b2cf9441d8952e4c50bd8bf1e46d69fa
hyehyeon-moon <hyehyeon.moon@linecorp.com>
2023-07-14 00:00:59 +0900

    Fix: Remove unnecessary back button on notification

--- src/app/notification/layout.tsx
+++ src/app/notification/layout.tsx
@@ -12,10 +12,6 @@ const NotificationLayout = ({ children }: PropsWithChildren) => {
             <h1 className="text-h1">알림</h1>
           </div>
         </TopNavigation.Left>
-        <TopNavigation.Title />
-        <TopNavigation.Right>
-          <TopNavigation.BackButton backButtonType="cancel" direction="right" />
-        </TopNavigation.Right>
       </TopNavigation>
       <div className="mb-10pxr mt-5 px-5 pb-b-nav">{children}</div>
       <BottomNavigation />
