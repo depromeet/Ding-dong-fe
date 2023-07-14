8ae59d8c78b1f587c3ef57799478a010c5a5681f
yunho <kimyouknow@naver.com>
2023-07-14 22:23:57 +0900

    feat: useCommunityStore에 isInitPlanetId추가

--- src/stores/community.store.ts
+++ src/stores/community.store.ts
@@ -3,14 +3,19 @@ import { create } from 'zustand';
 type Store = {
   communityId: number;
   switchCommunity: (id: number) => void;
+  isInitPlanetId: () => boolean;
 };
+const INIT_PLANET_ID = -1;
 
-export const useCommunityStore = create<Store>()(set => ({
-  communityId: -1,
+export const useCommunityStore = create<Store>()((set, get) => ({
+  communityId: INIT_PLANET_ID,
   switchCommunity: (id: number) => {
     document.cookie = `communityId=${id}`;
     set(() => ({
       communityId: id,
     }));
+  },
+  isInitPlanetId: () => {
+    return get().communityId === INIT_PLANET_ID;
   },
 }));
