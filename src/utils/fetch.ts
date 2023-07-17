62e07e8f1adaa458be1fae96dfc9193e1bf0282d
hyehyeon-moon <hyehyeon.moon@linecorp.com>
2023-07-18 00:27:10 +0900

    Feat: Add get and post fetch util

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,12 +1,43 @@
 import { ROOT_API_URL } from '~/api/config/requestUrl';
 
-export const getFetch = async (path: string, requestConfig?: RequestInit) => {
+export const getFetch = async (path: string, accessToken: string, requestConfig?: RequestInit) => {
   const url = `${ROOT_API_URL}${path}`;
   const options: RequestInit = {
     method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
+    headers: new Headers({
+      'content-type': 'application/json',
+      Authorization: `Bearer ${accessToken}`,
+    }),
+    mode: 'no-cors',
+    credentials: 'include',
+    ...requestConfig,
+  };
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  if (res.ok) {
+    return data;
+  } else {
+    throw Error(data);
+  }
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const postFetch = async (
+  path: string,
+  accessToken: string,
+  body?: Record<string, any>,
+  requestConfig?: RequestInit,
+) => {
+  const url = `${ROOT_API_URL}${path}`;
+  const options: RequestInit = {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: new Headers({
+      'content-type': 'application/json',
+      Authorization: `Bearer ${accessToken}`,
+    }),
+    mode: 'no-cors',
     credentials: 'include',
     ...requestConfig,
   };
