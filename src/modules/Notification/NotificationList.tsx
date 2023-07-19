374364366ea672fa8e8083294112f8b501633782
darae07 <leedr0730@gmail.com>
2023-07-20 00:02:45 +0900

    feat: 제목 목록 없을때 대응

--- src/modules/Notification/NotificationList.tsx
+++ src/modules/Notification/NotificationList.tsx
@@ -4,14 +4,14 @@ import { NotificationItem } from './NotificationItem';
 
 type NotificationListProps = {
   notifications: NotificationModel[];
-  notificationAgoList: Record<number, string>;
+  notificationAgoList?: Record<number, string>;
 };
 export const NotificationList = ({ notifications, notificationAgoList }: NotificationListProps) => {
   return (
     <ul className="flex flex-col gap-7">
       {notifications.map((notification, i) => (
         <div key={notification.notificationId}>
-          {!!notificationAgoList[notification.notificationId] && (
+          {!!notificationAgoList?.[notification.notificationId] && (
             <>
               {i !== 0 && <div className="-ml-5 mb-5 w-screen border-b border-b-gray-200"></div>}
               <h2 className="mb-4 text-h4">{notificationAgoList[notification.notificationId]}</h2>
