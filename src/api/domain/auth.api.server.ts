62fedbb641ec807e60abeaa48a25718642de3191
darae lee <61297852+darae07@users.noreply.github.com>
2023-07-22 04:56:05 +0900

    fix: 서버 reissue api header (#363)

--- src/api/domain/auth.api.server.ts
+++ src/api/domain/auth.api.server.ts
@@ -3,4 +3,6 @@ import { AuthResponse } from '~/types/auth';
 import publicApi from '../config/publicApi';
 
 export const reissue = async (refreshToken: string) =>
-  publicApi.get<AuthResponse>('/auth/login/reissue', { params: { REFRESH_TOKEN: refreshToken } });
+  publicApi.get<AuthResponse>('/auth/login/reissue', {
+    headers: { 'REFRESH-TOKEN': refreshToken },
+  });
