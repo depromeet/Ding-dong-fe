8f044e6b82df8d20897d3f562d2731b6feb964ee
yunho <kimyouknow@naver.com>
2023-07-22 09:45:09 +0900

    refactor: 소속된 행성이 없는 페이지에서 상단 회색 여백 제거

--- src/app/planet/page.tsx
+++ src/app/planet/page.tsx
@@ -17,7 +17,7 @@ const PlanetPage = () => {
           </Link>
         </TopNavigation.Right>
       </TopNavigation>
-      <div className="mt-26pxr flex flex-col justify-center px-layout-sm">
+      <div className="flex flex-col justify-center px-layout-sm pt-26pxr">
         <h1 className="mb-12pxr mt-4pxr text-h2 text-grey-900">아직 소속된 행성이 없네요!</h1>
         <p className="text-b2 text-grey-700">행성을 만들거나 초대된 행성으로 이동해보세요.</p>
         <Image src="/assets/images/planet.png" alt="planet" width={400} height={400} />
