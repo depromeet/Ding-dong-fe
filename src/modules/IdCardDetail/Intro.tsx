80972a9326ac3a78a9b1669e679376c0f94dbffb
darae07 <leedr0730@gmail.com>
2023-08-26 22:51:14 +0900

    style: 주민증 상단 UI 배치 변경사항 반영

--- src/modules/IdCardDetail/Intro.tsx
+++ src/modules/IdCardDetail/Intro.tsx
@@ -22,24 +22,24 @@ export const Intro = ({
   const bgColor = bgColors[characterType];
 
   return (
-    <div className="flex justify-between gap-10pxr px-21pxr pb-25pxr pt-8pxr">
-      <div className={`${bgColor} flex flex-col gap-[9px]`}>
-        <p className="text-h3">{nickname}</p>
-        <p className="text-b3 text-grey-600">{aboutMe}</p>
+    <div className="px-21pxr pb-25pxr pt-8pxr">
+      <div className={`${bgColor} flex flex-col items-center`}>
+        <div className="mb-10pxr h-80pxr w-80pxr flex-shrink-0 overflow-hidden rounded-full">
+          <Image
+            width={80}
+            height={80}
+            src={profileImageUrl}
+            alt="profile image"
+            className="max-h-[88px] min-h-[88px] min-w-[88px] max-w-[88px] rounded-full border-[1px] border-solid border-grey-100 object-cover"
+          />
+        </div>
+        <p className="mb-2 text-h4">{nickname}</p>
+        <p className="mb-4 text-center text-b3 text-grey-600">{aboutMe}</p>
         <div className="flex max-h-14 flex-wrap gap-6pxr overflow-hidden">
           {keywords.map(keyword => (
             <Tag key={keyword.title} type={characterType} label={keyword.title} />
           ))}
         </div>
-      </div>
-      <div className="h-80pxr w-80pxr flex-shrink-0 overflow-hidden rounded-full">
-        <Image
-          width={80}
-          height={80}
-          src={profileImageUrl}
-          alt="profile image"
-          className="max-h-[88px] min-h-[88px] min-w-[88px] max-w-[88px] rounded-full border-[1px] border-solid border-grey-100 object-cover"
-        />
       </div>
     </div>
   );
