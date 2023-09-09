61091b817bd4c53214387f73d33e92dc2a37ca44
99-zziy <wldud8229@gmail.com>
2023-09-10 00:54:14 +0900

    feat: nudge api 추가

--- src/api/domain/nudge.api.client.ts
+++ src/api/domain/nudge.api.client.ts
@@ -1,12 +1,14 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
 import privateApi from '~/api/config/privateApi';
 import { idCardQueryKey } from '~/api/domain/idCard.api';
 import { useToastMessageStore } from '~/stores/toastMessage.store';
 import { NudgePostRequest } from '~/types/nudge/request.type';
+import { NudgeListResponse } from '~/types/nudge/response.type';
 
 export const nudgeQueryKey = {
   send: (userId: number) => ['nudge', userId],
+  list: (idCardsId: number) => ['nudgeList', idCardsId],
 };
 
 export const usePostNudge = (userId: number, idCardId: number, nickname: string) => {
@@ -21,5 +23,14 @@ export const usePostNudge = (userId: number, idCardId: number, nickname: string)
       successToast(`${nickname}에게 성공적으로 딩동을 보냈어요!`);
     },
     onError: () => errorToast('콕 찌르기에 실패했습니다. 다시 시도해 주세요.'),
+  });
+};
+
+export const getNudgeList = (idCardsId: number) =>
+  privateApi.get<NudgeListResponse>(`/nudges/id-cards/${idCardsId}`);
+
+export const useGetNudgeList = (idCardsId: number) => {
+  return useQuery(nudgeQueryKey.list(idCardsId), () => getNudgeList(idCardsId), {
+    retry: false,
   });
 };
