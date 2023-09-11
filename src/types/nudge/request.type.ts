037b8589d09553c3487cfbcdad44dba32476c4c6
hyehyeon-moon <hyehyeon.moon@linecorp.com>
2023-09-12 00:15:23 +0900

    Add nudge put api

--- src/types/nudge/request.type.ts
+++ src/types/nudge/request.type.ts
@@ -1,3 +1,5 @@
 import { NudgeModel } from '~/types/nudge/model.type';
 
 export type NudgePostRequest = { nudgeType: NudgeModel; communityId: number };
+
+export type NudgePutRequest = NudgePostRequest;
